f4e55743784b1b9ef151982d4504d1509459f29a
ImrenR <imrenrahbay@gmail.com>
2025-11-23 17:00:54 +0300

    update

---
+++ src/j01_Variables/C01_HelloWord.java
@@ -0,0 +1,71 @@
+package j01_Variables;
+
+
+/*
+Selam javaCAN-> yorum satırının yeri ve sınırı yoktur...
+ */
+
+public class C01_HelloWord {// Class level
+
+    public static void main(String[] args) {//main meth level
+        // java yorum satırlarını -> comment line compile(derleme) yapmaz
+        // -> tek satır yorum
+
+
+        /*-> çoklu satır : multiple comment line
+        bu araya ne yazarsan yaz derlenmez.
+        ahanda trick-> madem geldin dünyaya
+        otur çalış javaya
+        clarusda adamı javaylan döverler
+         */
+
+
+        /*
+
+        haluk der ki : bahanesi olanın başarısı olmaz olamazzzz
+         */
+
+        //imleçin(cursor) oldugu satır ctrl+d cmd+d ile alt satıra kopyalanır.
+        //imleçin(cursor) oldugu satır ctrl+z cmd+z ile önceki actiona geri alınır..
+
+        /*
+        Java run edildiğinde çalışmaya ilk önce main method'dan başlar.
+        daha sonra yukarıdan aşağıya ve soldan sağa soğru sırasıyla code'ları çalıştırır.
+         */
+        System.out.println("Hello World :) ");//console output->çıktı veren komut
+        System.out.print("Hello World :) ");//console output->çıktı veren komut
+        System.out.println(" javaCAN'lara selam olsun ");//console output->çıktı veren komut
+        // ctrl+alt+L tıklanarak komut satırları formatlanır.
+/*
+TRICK ->
+print() :yazdırma işleminden sonra komut AYNI satırdan devam ettirir
+println() :yazdırma işleminden sonra komut ALT satırdan devam ettirir
+java'da space(boşluk) bir karakter olarak tanımlanır()derlenir
+javaCAN-> 7 karakter
+j a v a C A N->13 karakter
+ */
+
+        // sout yazılıp enter basılırsa -> System.out.println(); print komutu  tanımlanır
+      /*
+      console output(ekran çıktısı) için 2 komut kullanılır
+      1...  System.out.println(); => satır başı yapar
+      2... System.out.print(); => satır başı yapmaz
+       */
+
+
+
+
+    }//main sonu
+    }//main sonu
+
+
+
+/*
+selamjavaCAN-> main meth. dışına da yarum yazılabilir:"başarı gayrete aşıktır."
+ */
+
+
+//Class sonu
+
+
+//selam javaCAN-> class dışına da yorun satırı yazılabilir...
