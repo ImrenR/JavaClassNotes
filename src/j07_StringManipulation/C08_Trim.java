62ab976bdecbb5247a9c9c83c631a56e85effb5a
ImrenR <imrenrahbay@gmail.com>
2025-11-23 19:24:38 +0300

    up

--- src/j07_StringManipulation/C08_Trim.java
+++ src/j07_StringManipulation/C08_Trim.java
@@ -1,13 +1,22 @@
 package j07_StringManipulation;
 
 public class C08_Trim {
+
     public static void main(String[] args) {
 
            /*
   trim()-> İstediğimiz String’in başında veya sonunda varolan boşluk (space)’leri temizler.
-
+  Does not remove spaces between words.
+  Does modify the original string
+Only removes spaces at the start and end of the string.
          */
+String str1 = "   Hello World   ";
+String str2 = "Java    ";
 
+String trimmedStr1 = str1.trim();
+String trimmedStr2 = str2.trim();
+        System.out.println(trimmedStr2);
+        System.out.println(trimmedStr1);
 
     }
 }
