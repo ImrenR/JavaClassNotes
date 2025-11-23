e5d9d1de5024b469d537bb3a9d4fde18183486a5
ImrenR <imrenrahbay@gmail.com>
2025-11-23 17:24:59 +0300

    endsWith

--- src/j07_StringManipulation/C05_Starts_EndsWith.java
+++ src/j07_StringManipulation/C05_Starts_EndsWith.java
@@ -1,5 +1,7 @@
 package j07_StringManipulation;
 
+import java.util.Scanner;
+
 public class C05_Starts_EndsWith {
     public static void main(String[] args) {
             /*
@@ -9,8 +11,25 @@ public class C05_Starts_EndsWith {
           endsWith()
           Metnin son kisminin istenilen dizi ile eslesip eslesmedigini kontrol eder. Sonucu boolean dir.
          */
+String str = "Hello Java";
 
+//startWith() ornegi
+        System.out.println(str.startsWith("Hello"));
+        System.out.println(str.endsWith("va"));
         //Task-> girilen e-mail hesabının  @gmail.com içermiyorsa "lütfen gmail hesabı giriniz"
         // @gmail.com ile  bitiyorsa "hesabınız onaylandı" aksi durumda "geçerli hesap giriniz print eden code create ediniz.
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your email");
+        String myEmail= input.nextLine();
+
+    if (!myEmail.contains("@gmail.com")){
+        System.out.println("lutfen gmail hesabi giriniz");
+    }else if (myEmail.endsWith("@gmail.com")){
+        System.out.println("Hesabiniz onaylandi");
+    }else {
+        System.out.println("Gecerli hesap giriniz");
+    }
+
     }
 }
