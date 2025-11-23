16eafc5b5cd0e8ffd19fdcf72ea6c57e291a22dd
ImrenR <imrenrahbay@gmail.com>
2025-11-23 20:01:59 +0300

    up

--- src/j07_StringManipulation/C09_UpperCaseLowerCase.java
+++ src/j07_StringManipulation/C09_UpperCaseLowerCase.java
@@ -1,5 +1,7 @@
 package j07_StringManipulation;
 
+import java.util.Locale;
+
 public class C09_UpperCaseLowerCase {
     public static void main(String[] args) {
           /*
@@ -7,6 +9,18 @@ public class C09_UpperCaseLowerCase {
        toLowerCase()-> verilen string'i tum harflerini kucuk harfe çevirir.
        ignoreCase()-> verilen string'i BUYUK-kucuk harf duyarlılığını kaldırır.
          */
+String str = "Java Can";
+String upperStr = str.toUpperCase();
+        System.out.println(upperStr);
+String str1 = "JAVa Can";
+String lowerCase = str1.toLowerCase();
+        System.out.println(lowerCase);
 
+
+        String str2 = "JAVA";
+        String str3 = "java";
+
+        System.out.println(str2.equals(str3)); // case sensetive
+        System.out.println(str2.equalsIgnoreCase(str3)); // true ignores cases
     }
 }
--- src/j07_StringManipulation/C10_indexOf_lastIndexOf.java
+++ src/j07_StringManipulation/C10_indexOf_lastIndexOf.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 
 public class C10_indexOf_lastIndexOf {
     public static void main(String[] args) {
-               /* indexOf()->aranan yolcunun koltuk numarasını verir
+               /* indexOf()-> aranan yolcunun koltuk numarasını verir.
           char karakter ya da String metnin kacinci indexden basladigini veren bir method dur. Sonuc integer dir.
           Bu method index'i sorulan char'in String icindeki ilk gorunumunun index'ini return eder
           contains den farkı indexini verir, contains ise true false
+          donus tipi : int tir.
+          Eger karakter string varsa ilk buldugu index i doner.
+          Eger yoksa -1 doner CTE veya RTE vermez.
+          Contains() gibi true false yerine index numarasi verir.
         */
 
-
+String str = " Java is fun";
+        System.out.println(str.indexOf("a"));
+        System.out.println(str.indexOf("is"));
+        System.out.println(str.indexOf("Python"));
 
         // ahan da trick-> indexOf("Java"); olmayan char veya String için CTE veya RTE değil -1 return eder
 
@@ -20,6 +27,8 @@ public class C10_indexOf_lastIndexOf {
          lastIndexOf() Method'u index'i sorulan character'in son gorunumunun index'ini return eder.
          indexOf() methodunda birden fazla character'in index'i sorulursa ilk character'in index'i return edilir.
          */
+
+
 
          /* Task-> girilen bir string ifadenin aşağıdaki şartları sağlamasını kontrol eden code create ediniz
         a-) girilen ifade java içermiyor
