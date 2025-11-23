8d9884d25530131728bdb5e66c29b0239f41df8f
ImrenR <imrenrahbay@gmail.com>
2025-11-23 17:40:13 +0300

    update

--- src/j07_StringManipulation/C06_Substring.java
+++ src/j07_StringManipulation/C06_Substring.java
@@ -13,11 +13,17 @@ public class C06_Substring {
         baslangic indexi ==> inclusive/dahil
         bitis indexi ==> exclusive/haric
          */
-
+String str = "JavaGuzelsin" ;
+        System.out.println(str.substring(4)); //Guzelsin , Index 4 ten sonuna kadar
+        System.out.println(str.substring(0,4)); // 0 dahil, 4 haric. Java
 
         // task-> girilen 4 harfli bir kelimenin tersten print eden code create ediniz. -> kale:elak
 
-
+Scanner input = new Scanner(System.in);
+        System.out.println(" enter a word");
+        String newWord = input.nextLine();
+     String yeniKelime = newWord.substring(3) + newWord.substring(2,3) + newWord.substring(1,2) + newWord.substring(0,1);
+        System.out.println(yeniKelime);
 
     }
 }
