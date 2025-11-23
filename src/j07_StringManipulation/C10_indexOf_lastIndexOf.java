1a790511ae2ef1935656e7cdeab109f7d1f92707
ImrenR <imrenrahbay@gmail.com>
2025-11-23 20:10:59 +0300

    update

--- src/j07_StringManipulation/C10_indexOf_lastIndexOf.java
+++ src/j07_StringManipulation/C10_indexOf_lastIndexOf.java
@@ -26,9 +26,14 @@ String str = " Java is fun";
          indexOf un sondan olan hali fakat index numaraları değişmez.
          lastIndexOf() Method'u index'i sorulan character'in son gorunumunun index'ini return eder.
          indexOf() methodunda birden fazla character'in index'i sorulursa ilk character'in index'i return edilir.
+         Aranan karakter veya string in son bulunusunun indeksini verir.
+         Bastan saymaya devam eder yani indeksler degismez
+         indexOf() ile farki indexOf ilk bulunus lastIndex() son bulunusu verir
          */
 
-
+String strX = "Java is fun and easy";
+        System.out.println(strX.indexOf("Java")); // 0 ilk java
+        System.out.println(strX.lastIndexOf("Java")); // 15 son java
 
          /* Task-> girilen bir string ifadenin aşağıdaki şartları sağlamasını kontrol eden code create ediniz
         a-) girilen ifade java içermiyor
@@ -36,8 +41,16 @@ String str = " Java is fun";
         c-) girilen ifade  1'den çok java içeriyor
          */
 
-
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir string giriniz: ");
+        String sc = input.nextLine().toLowerCase();
+if(sc.indexOf("java") == -1) {
+    System.out.println("Java icermiyor");
+} else if (sc.indexOf("java") == sc.lastIndexOf("java")) {
+    System.out.println("Bir adet java iceriyor");
+}else {
+    System.out.println("birden fazla java var");
+}
 
     }
 }
