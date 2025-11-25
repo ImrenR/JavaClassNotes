47c61cdb053867e67ca45057703153a8b0b6c1e5
ImrenR <imrenrahbay@gmail.com>
2025-11-25 21:10:06 +0300

    task completed

--- src/j07_StringManipulation/tasks/Task01.java
+++ src/j07_StringManipulation/tasks/Task01.java
@@ -6,7 +6,8 @@ public class Task01 {
     public static void main(String[] args) {
         // Task->  Girilen  bir stringde boşluk karakterinin olup olmadığını kontrol eden code create ediniz
 
-
+String kelime = "I love you a lot";
+        System.out.println("kelime has empty space ?= " + kelime.contains(" ")); // true
     }
 }
 
--- src/j07_StringManipulation/tasks/Task02.java
+++ src/j07_StringManipulation/tasks/Task02.java
@@ -6,11 +6,33 @@ public class Task02 {
     public static void main(String[] args) {
         /*  Task->
 		 	Girilen passwordun aşagıdaki şartları sağlamasını kontrol eden coden create ediniz
-		 	 a)Ilk karakteri buyuk harf olmalı
+		 	 a)Ilk karakteri buyuk harf olmalı //
 		 	 b)Son karakteri sayi olamlı
 		 	 c)en az 6 karakter uzunlugunda olmalı
-
 		 */
+
+Scanner input = new Scanner(System.in);
+        System.out.println("enter a valid  password:");
+        String password = input.nextLine();
+
+        boolean ilkBuyuk = Character.isUpperCase(password.charAt(0));
+        boolean sonBuyuk = Character.isDigit(password.charAt(password.length()-1));
+        boolean uzunluk = password.length()<=6;
+
+        if(ilkBuyuk && sonBuyuk && uzunluk) {
+            System.out.println("Password is valid");
+        }else {
+            System.out.println("Password is invalid");
+            if(!ilkBuyuk){
+                System.out.println("First character has to be capital");
+            }
+            if(!sonBuyuk){
+                System.out.println("Last character has to be a number");
+            }
+            if(!uzunluk){
+                System.out.println("Password has to be less than 6 characters");
+            }
+        }
 
     }
 }
