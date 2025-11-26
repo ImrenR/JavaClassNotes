b2ce68197b71972b4383ad79344b03365eb73626
ImrenR <imrenrahbay@gmail.com>
2025-11-26 18:51:14 +0300

    update

--- src/j10_MethodCreation/C03_MethodCreation.java
+++ src/j10_MethodCreation/C03_MethodCreation.java
@@ -3,10 +3,22 @@ package j10_MethodCreation;
 
 
 public class C03_MethodCreation {//Class level->Benim koyum
+
     public static void main(String[] args) {//main level
 
+        String imren="en guzel JavaCan";
+        String haluk="backend developer";
+        selamVer(); //kendi koyumun amele meth call edildi..
+        System.out.println(imren);
+        System.out.println(haluk);
+        C03_MethodDepo.gecmeNotu(77); //ClassName ile komsu koyden meth call edildi...
 
     }//main sonu
+
+    private static void selamVer() {//kendi koyumun amele metodu
+
+        System.out.println("koyumun yagmurlarinda yikasinlar!");
+    }
 
 
 }//Clas sonu
