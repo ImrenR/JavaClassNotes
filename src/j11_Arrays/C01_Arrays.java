8ffaa30d5b2e534d739f386981ddd5ae9b497dde
ImrenR <imrenrahbay@gmail.com>
2025-11-27 21:50:45 +0300

    method creation

--- src/j11_Arrays/C01_Arrays.java
+++ src/j11_Arrays/C01_Arrays.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class C01_Arrays {
     public static void main(String[] args) {
+
             /*
         Array (Dizi): Java'da birden fazla aynı type variable depolamak (store etmek) icin kullanilan bir objedir.
 
@@ -19,8 +20,8 @@ public class C01_Arrays {
         //Array declaration
 
  int a; //declere edilmis ama assignment edilmemit primitive data
-int [] arr; // declare edilmis ama assignment edilmemis non-primitive int data type array
-// Trick => declare edilmis ama assignment edilmemeis array herhangi bir actiona a alinirsa CTE verir.
+ int [] arr; // declare edilmis ama assignment edilmemis non-primitive int data type array
+ // Trick => declare edilmis ama assignment edilmemeis array herhangi bir actiona a alinirsa CTE verir.
 
         String isimArr[]= {"imren", "haluk", "tulin", "erdem", "hidayet", "ali haydar", "mehmet"};
         int sayiArr1[]=new int[5]; //eleman sayisi 5 declare edilen bos int data type array
@@ -33,13 +34,27 @@ sayiArr1[0]=20; //1. eleman
 sayiArr1[2]=31;  // 3. eleman
 sayiArr1[1]=35; // 2. eleman
 sayiArr1[3]=34; // 4. eleman
-sayiArr1[1]= 28; // 2. elaman 35 olan deger 28 olarak update edildi.
+sayiArr1[1]= 28; // 2 . elaman 35 olan deger 28 olarak update edildi.
+//sayiArr1[10] = 34; // RTE run time error verir cunku eleman sayisi 10 dan kucuktur.
         // Array elemanlari benzersiz olmak zorunda degildir.
+
 
         // Array eleman  sayısı-> length
 
+ System.out.println("isimArr.length = " + isimArr.length); // length bize eleman sayisini verir
+
+
         //Array print etme...
 
+        System.out.println("isimArr = " + isimArr); // isimArr =[Ljava.lang.String;@8efb846
+        // array bir obj oldugu icin bize bir referans degeri donderir.
+       // 1.yontem : System.out.println("isimArr = " + Arrays.toString(isimArr)); // isimArr = [imren, haluk, tulin, erdem, hidayet, ali haydar, mehmet]
+// butun elemanlarina ulasmak istiyorsan Arrays.toString() metoduyla ulasirsin.
+      // 2. yontem :
+        for (int i = 0; i < isimArr.length; i++) {
+            System.out.print(isimArr[i] + " ");
+        }
+        // onemli not ! 1.yontem bize array yazdirir ikinci yontem de array i kaldirir
 
         // Array son index eleman  -> arr[arr.length-1]
 
