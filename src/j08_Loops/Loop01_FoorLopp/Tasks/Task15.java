c749db9b9628eecadee16358a668f930e805b587
ImrenR <imrenrahbay@gmail.com>
2025-11-28 00:41:35 +0300

    update

--- src/j08_Loops/Loop01_FoorLopp/Tasks/Task15.java
+++ src/j08_Loops/Loop01_FoorLopp/Tasks/Task15.java
@@ -1,9 +1,23 @@
 package j08_Loops.Loop01_FoorLopp.Tasks;
 
+import java.util.Scanner;
+
 public class Task15 {
 	public static void main(String[] args) {
 		/*
 		Task-> girilen bir String'deki rakam sayısını print eden code create ediniz
 		 */
+        Scanner input= new Scanner(System.in);
+        System.out.println("Bir string giriniz:");
+        String word= input.nextLine();
+
+//char ->  tek bir karakteri baz alir
+        // char ch = word.charAt(i) ise kelimenin uzunlugu kadar donecek ve toplayacaktir
+        int rakamSayisi = 0;
+        for (int i = 0; i <word.length() ; i++) {
+            char ch =word.charAt(i);
+            rakamSayisi++;
+        }
+        System.out.println("String icindeki rakam sayisi : " + rakamSayisi);
 	}
 }
--- src/j08_Loops/Loop01_FoorLopp/Tasks/Task16.java
+++ src/j08_Loops/Loop01_FoorLopp/Tasks/Task16.java
@@ -5,12 +5,35 @@ import java.util.Scanner;
 public class Task16 {
 
     /*
-     task-> Girilen sayının asal olmasını kontrol eden code create ediniz.
+     task-> Girilen sayının asal(prime) olmasını kontrol eden code create ediniz.
      * */
     public static void main(String[] args) {
 
 
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int num = input.nextInt();
+
+        boolean isPrime = true; //we assumed the number is prime at first
+
+        if(num <2){
+            isPrime = false; // number less than 2 is not a prime
+
+        }else {
+            for (int i = 2; i <=Math.sqrt(num) ; i++) { // check all possible divisors up to sprt num
+                if (num%i == 0) {
+                    isPrime=false;
+                    break;
+                }
+
+            }
+        }
+    if (isPrime){
+        System.out.println(num + " is a prime number");
+    }else System.out.println(num + " is not a prime number");
+
+
+
 
     }
 }
