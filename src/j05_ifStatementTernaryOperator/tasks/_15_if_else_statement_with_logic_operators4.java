c2d6fb304429b882cfcaf68bb0d0ada2976e5d8f
ImrenR <imrenrahbay@gmail.com>
2025-11-27 22:13:48 +0300

    ifelsestatement tasks

--- src/j05_ifStatementTernaryOperator/tasks/_15_if_else_statement_with_logic_operators4.java
+++ src/j05_ifStatementTernaryOperator/tasks/_15_if_else_statement_with_logic_operators4.java
@@ -12,11 +12,17 @@ public class _15_if_else_statement_with_logic_operators4 {
              Aksi halde (else) "no money" yazdırın.   */
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("bir sayi giriniz :");
         String number = scan.nextLine();
 
         //Kodu aşağıya yazınız. BU SATIRIN YUKARISINI DEĞİŞTİRMEYİNİZ.
 
+        if(number.contains("€")){
+            System.out.println("This is euro");
 
+        } else if (number.contains("$")) {
+            System.out.println("This is dollar");
+        }else System.out.println("no money");
 
 
     }
