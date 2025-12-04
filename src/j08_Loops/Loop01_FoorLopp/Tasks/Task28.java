4ee2396af6cf86fff05238f9a725d5b738e8564e
ImrenR <imrenrahbay@gmail.com>
2025-12-04 21:36:03 +0300

    update

--- src/j08_Loops/Loop01_FoorLopp/Tasks/Task28.java
+++ src/j08_Loops/Loop01_FoorLopp/Tasks/Task28.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class Task28 {
     public static void main(String[] args) {
 
-        // task-> girilen  bir char karakterden sonra gelen 10 karakteri prin eden code create ediniz
+        // task-> girilen  bir char karakterden sonra gelen 10 karakteri print eden code create ediniz
 
+Scanner input = new Scanner(System.in);
+        System.out.println("Bit char giriniz: ?");
+        char harf = input.next().charAt(0);
 
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println((char)(harf+i) + " ");
+        }
     }
 }
