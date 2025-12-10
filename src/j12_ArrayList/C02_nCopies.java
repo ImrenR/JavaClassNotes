43eed121f75a3df7701a56b56473765aa4f4c6f3
ImrenR <imrenrahbay@gmail.com>
2025-12-10 17:21:56 +0000

    nCopies

--- src/j12_ArrayList/C02_nCopies.java
+++ src/j12_ArrayList/C02_nCopies.java
@@ -6,10 +6,18 @@ import java.util.Collections;
 
 public class C02_nCopies {
     public static void main(String[] args) {
+
+
+ArrayList<String> ulkeList =new ArrayList<>(Arrays.asList("almanya" , "Cuba", "Ingiltere"));
+ArrayList<String> isimList =new ArrayList<>(Arrays.asList("imren" , "elvan", "haluk"));
+
         //nCopies(int n, Obj)-> n elemanlı sabit obj'den oluşan list tanımlar
+ArrayList<String>yeniList = new ArrayList<>(Collections.nCopies(7, "JavaCan")); // 7 JavaCan elemanli list tanimlandi.
+        System.out.println(yeniList);
 
 
         //addAll(list);-> girilen liste istene liste ekler..
+
 
     }
 }
