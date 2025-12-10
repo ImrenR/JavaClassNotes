9be087544729468eb086076f3d368136f782a8c9
ImrenR <imrenrahbay@gmail.com>
2025-12-10 17:58:23 +0000

    remove

--- src/j12_ArrayList/C11_remove.java
+++ src/j12_ArrayList/C11_remove.java
@@ -7,6 +7,12 @@ public class C11_remove {
     public static void main(String[] args) {
         //remove()-> list'te istenen elemanı siler
 
+        ArrayList<String> ulkeList =new ArrayList<>(Arrays.asList("almanya" , "Cuba", "Ingiltere"));
+        System.out.println("Remove oncesi ulkeList = " + ulkeList);
+        System.out.println("ulkeList.remove(2) = " + ulkeList.remove(2));
+        System.out.println("ulkeList.remove(\"Tanzanya\") = " + ulkeList.remove("Tanzanya"));
+        System.out.println("ulkeList.remove(\"Cuba\") = " + ulkeList.remove("Cuba"));
+        System.out.println("Remove sonrasi ulkeList = " + ulkeList); // cikarilmis listeyi verir bastaki liste gelmez.
 
     }
 }
