ecbd138efde39e459319243f1f2950f2139d51f9
ImrenR <imrenrahbay@gmail.com>
2025-12-10 17:41:22 +0000

    isEmpty

--- src/j12_ArrayList/C06_isEmpty_Clear.java
+++ src/j12_ArrayList/C06_isEmpty_Clear.java
@@ -6,10 +6,17 @@ import java.util.Arrays;
 public class C06_isEmpty_Clear {
     public static void main(String[] args) {
         //isEmpty()-> listin boş olmasını kontrol eder true/false return eder
+        ArrayList<String> ulkeList =new ArrayList<>(Arrays.asList("almanya" , "Cuba", "Ingiltere"));
+        ArrayList<String> sehirList =new ArrayList<>();
+
+        System.out.println("ulkeList.isEmpty() = " + ulkeList.isEmpty()); //false
+        System.out.println("sehirList.isEmpty() = " + sehirList.isEmpty()); // true
 
 
         //clear()-> listin tum elemanlarını silip boşaltır...
-
+        System.out.println("ulkeList.size() = " + ulkeList.size());
+       ulkeList.clear(); // list bosaltildi // sout olmaz burada
+        System.out.println("ulkeList.size() = " + ulkeList.size()); // 0
 
     }
 }
