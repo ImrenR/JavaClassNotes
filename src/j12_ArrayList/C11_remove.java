4467a154e380405baa310afb9357f055146051d8
ImrenR <imrenrahbay@gmail.com>
2025-12-10 17:59:44 +0000

    removeAll

--- src/j12_ArrayList/C11_remove.java
+++ src/j12_ArrayList/C11_remove.java
@@ -11,8 +11,10 @@ public class C11_remove {
         System.out.println("Remove oncesi ulkeList = " + ulkeList);
         System.out.println("ulkeList.remove(2) = " + ulkeList.remove(2));
         System.out.println("ulkeList.remove(\"Tanzanya\") = " + ulkeList.remove("Tanzanya"));
+       // System.out.println("ulkeList.remove(5) = " + ulkeList.remove(5)); // error verir cunku listede 5 adet eleman yoktur
         System.out.println("ulkeList.remove(\"Cuba\") = " + ulkeList.remove("Cuba"));
         System.out.println("Remove sonrasi ulkeList = " + ulkeList); // cikarilmis listeyi verir bastaki liste gelmez.
+
 
     }
 }
