6bd4fe6bfc2c88569885117ab5f36a812bedddeb
ImrenR <imrenrahbay@gmail.com>
2025-12-10 17:36:52 +0000

    size

--- src/j12_ArrayList/C04_get.java
+++ src/j12_ArrayList/C04_get.java
@@ -7,5 +7,9 @@ public class C04_get {
     public static void main(String[] args) {
         //get()-> listte girilen index'deki elemanı return eder. olmayan index için RTE exc. fırlatır
 
+
+        ArrayList<String> ulkeList =new ArrayList<>(Arrays.asList("almanya" , "Cuba", "Ingiltere"));
+        System.out.println("ulkeList.get(3) = " + ulkeList.get(2));
+        // System.out.println(ulkeList.get(8)); // eror verir cunku array de 8 adet eleman yok
     }
 }
--- src/j12_ArrayList/C05_size.java
+++ src/j12_ArrayList/C05_size.java
@@ -7,5 +7,10 @@ public class C05_size {
     public static void main(String[] args) {
         //size()-> listin eleman sayısını return eder.(arr->length)
 
+
+
+        ArrayList<String> ulkeList =new ArrayList<>(Arrays.asList("almanya" , "Cuba", "Ingiltere"));
+        System.out.println(ulkeList.size());
+
     }
 }
