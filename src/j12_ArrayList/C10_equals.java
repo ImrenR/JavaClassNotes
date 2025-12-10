81fdae14a7b2140d822f793ac6aa150412ab9837
ImrenR <imrenrahbay@gmail.com>
2025-12-10 17:55:26 +0000

    equals

--- src/j12_ArrayList/C10_equals.java
+++ src/j12_ArrayList/C10_equals.java
@@ -5,7 +5,11 @@ import java.util.Arrays;
 
 public class C10_equals {
     public static void main(String[] args) {
-        //equals()-> iki listin hem index hem de value (değer) eşitliğini kontrol eder true/false return eder
+        //equals()-> iki listin hem index hem de value (değer) eşitliğini kontrol eder
+        // true/false return eder
+        ArrayList<String> ulkeList1 =new ArrayList<>(Arrays.asList("almanya" , "Cuba", "Ingiltere", "Portekiz"));
+        ArrayList<String> ulkeList2 =new ArrayList<>(Arrays.asList("Cuba","almanya" ,"Portekiz" ,"Ingiltere"));
+        ArrayList<String> ulkeList3 =new ArrayList<>(Arrays.asList("almanya" , "Cuba", "Ingiltere", "Portekiz"));
 
 
 
