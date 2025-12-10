3a36b85c9e8b28de7e3465b5ee9e966b392f6e0a
ImrenR <imrenrahbay@gmail.com>
2025-12-10 17:42:32 +0000

    contains

--- src/j12_ArrayList/C07_contains.java
+++ src/j12_ArrayList/C07_contains.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class C07_contains {
     public static void main(String[] args) {
         //contains()-> list'te istenen elamnın varlığını control eder true/false return eder
-
+        ArrayList<String> ulkeList =new ArrayList<>(Arrays.asList("almanya" , "Cuba", "Ingiltere"));
+        System.out.println("ulkeList.contains(\"kolonya\") = " + ulkeList.contains("kolonya")); // false
     }
 }
