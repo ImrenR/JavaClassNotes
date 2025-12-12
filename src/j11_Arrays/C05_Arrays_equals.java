54bfccb4343c3b83021d7ca50646b07563491bd8
ImrenR <imrenrahbay@gmail.com>
2025-12-12 15:16:19 +0000

    equals

--- src/j11_Arrays/C05_Arrays_equals.java
+++ src/j11_Arrays/C05_Arrays_equals.java
@@ -8,9 +8,15 @@ public class C05_Arrays_equals {
 
         int arr1[] = {2, 5, 13, 65, 49, 38, 33, 58, 27};
         int arr2[] = {21, 52, 14, 63, 49, 31, 35, 53, 26};
+        int arr3[] = {2, 13, 5, 65, 38, 49, 33, 58, 27};
 
 
         // Array'ı String'e convert(çevirme)...
 
+        System.out.println("Arrays.equals(arr1,arr2) = " + Arrays.equals(arr1, arr2)); // false
+        System.out.println("Arrays.equals(arr1,arr3) = " + Arrays.equals(arr1, arr3)); //false
+        Arrays.sort(arr1);
+        Arrays.sort(arr3);
+        System.out.println("Arrays.equals(arr1,arr2) = " + Arrays.equals(arr1, arr3)); // true
     }
 }
