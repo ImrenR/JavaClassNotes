a647d1a42489d3a01fcd515732b83665812339d6
ImrenR <imrenrahbay@gmail.com>
2025-12-12 15:11:18 +0000

    split

--- src/j11_Arrays/C04_Arrays_split.java
+++ src/j11_Arrays/C04_Arrays_split.java
@@ -7,11 +7,23 @@ public class C04_Arrays_split {
         // String data type dataları Arrays'e elaman olarak cevirmek için split() meth kullanılır.
 
 
-        String  str="javaNAZ'lara bolcana selamkeee. taskları çözmeyenin klayveyisi bozulsun ekranı mavi caksın :( ";
+        String str = "javaNAZ'lara bolcana selamkeee. taskları çözmeyenin klayveyisi bozulsun ekranı mavi caksın :( ";
         // task-> str'nin kelime ve harf sayısını print eden code create ediniz.
+        // saymak nedir: count , count ta bir sart saglayacak ya if -> bos bir array a atarim
+        // ama her bir kelimede bir eleman olacagina gore birden cok elemani arrray a depolayacagima gore
+        // -> bos bir array atayacagim,array in eleman sayisini nasil bulacagim , saydirmam lazim
+        //-> ama boundunu(length) bilmiyorum cunku boundu soruluo, dolayisiyla bua
+        //->bos bir array tanimlayamam[] x
+        // print etmek : sout
 
+        String kelimeArr[] = str.split(" ");
+        System.out.println("str = " + str); //->cikti : javaNAZ'lara bolcana selamkeee. taskları çözmeyenin klayveyisi bozulsun ekranı mavi caksın :(
+        System.out.println("Arrays.toString(kelimeArr) = " + Arrays.toString(kelimeArr));
+        // cikti-> [javaNAZ'lara, bolcana, selamkeee., taskları, çözmeyenin, klayveyisi, bozulsun, ekranı, mavi, caksın, :(]
+        System.out.println("kelimeArr.length = " + kelimeArr.length); // 11 elemanli bir array
 
-
-
+        String harfSayisi[]=str.split("");
+        System.out.println("Arrays.toString(harfSayisi) = " + Arrays.toString(harfSayisi)); //cikti -> [j, a, v, a, N,  .. seklinde
+        System.out.println("harfSayisi.length = " + harfSayisi.length); // 94
     }
 }
