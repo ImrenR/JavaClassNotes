eb103ceed48b1d66ff5e39fae06700b7a1a2816b
ImrenR <imrenrahbay@gmail.com>
2025-12-15 21:18:54 +0000

    arrays

--- src/j11_Arrays/Tasks/_11_Average_of_Array.java
+++ src/j11_Arrays/Tasks/_11_Average_of_Array.java
@@ -10,6 +10,13 @@ public class _11_Average_of_Array {
          */
 
         //Kodu aşağıya yazınız.
+int [] numbers = {12,14,21,23,10,4};
+int toplam = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            toplam += numbers[i];
+        }
+        System.out.println("toplam/numbers.length = " + toplam / numbers.length);
 
     }
 }
