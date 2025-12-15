5953aeb85e84f2bec5d4320115d565d2af935529
ImrenR <imrenrahbay@gmail.com>
2025-12-15 20:29:18 +0000

    arrays

--- src/j11_Arrays/Tasks/_02_Create_array2.java
+++ src/j11_Arrays/Tasks/_02_Create_array2.java
@@ -1,5 +1,7 @@
 package j11_Arrays.Tasks;
 
+import java.util.Arrays;
+
 public class _02_Create_array2 {
 
     public static void main(String[] args) {
@@ -10,6 +12,9 @@ public class _02_Create_array2 {
      */
         // Kodu aşağıya yazınız..
 
+int[] eleman ={13,15,14,16,16};
 
+        System.out.println("Elemanlar array icinde =" + Arrays.toString(eleman));
+        //Elemanlari array icinde bastirmak Arrays.toString ile olur
     }
 }
