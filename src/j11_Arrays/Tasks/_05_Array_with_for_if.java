7dcd3a7ddd8453bb852586dbf79dc2181afe0085
ImrenR <imrenrahbay@gmail.com>
2025-12-15 20:39:28 +0000

    arraysTasks

--- src/j11_Arrays/Tasks/_05_Array_with_for_if.java
+++ src/j11_Arrays/Tasks/_05_Array_with_for_if.java
@@ -14,6 +14,28 @@ public class _05_Array_with_for_if {
 
         //Kodu aşağıya yazınız.
 
+int[] sayilar = {5,6,8,12,14,19};
+int toplam =0;
+
+        for (int i = 0; i < sayilar.length; i++) {
+           if (sayilar[i] % 2 == 0){ // cift sayilar
+               toplam += sayilar[i];
+           }else {
+               toplam -=sayilar[i];
+           }
+        }
+
+        System.out.println("toplam =" + toplam);
+
+
+
+
+
+
+
+
+
+
 
     }
 }
