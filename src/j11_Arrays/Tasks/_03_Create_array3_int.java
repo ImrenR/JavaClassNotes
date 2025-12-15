5cad9d6fcc47cead62268015c507d503ab752c79
ImrenR <imrenrahbay@gmail.com>
2025-12-15 20:32:08 +0000

    arrays

--- src/j11_Arrays/Tasks/_03_Create_array3_int.java
+++ src/j11_Arrays/Tasks/_03_Create_array3_int.java
@@ -11,6 +11,16 @@ public class _03_Create_array3_int {
 
         //Kodu aşağıya yazınız..
 
+int[] sayi= {25,30,30,35,100};
+
+int toplam=0;
+
+        for (int i = 0; i < sayi.length ; i++) {
+            toplam += sayi[i];
+        }
+
+        System.out.println("Sayilarin toplami:" + toplam);
+
 
     }
 }
