fbdae90b214793cae418ffa63be3f94021de8107
ImrenR <imrenrahbay@gmail.com>
2025-12-16 02:45:52 +0000

    arryaList

--- src/j12_ArrayList/C09_subList.java
+++ src/j12_ArrayList/C09_subList.java
@@ -9,12 +9,12 @@ public class C09_subList {
 
         ArrayList<String> ulkeList =new ArrayList<>(Arrays.asList("almanya" , "Cuba", "Ingiltere"));
         System.out.println("SubList oncesi = " + ulkeList);
-        System.out.println("ulkeList.subList(1,2) = " + ulkeList.subList(1, 2));
-        System.out.println("ulkeList sublist sonrasi = " + ulkeList);
+        System.out.println("ulkeList.subList(1,2) = " + ulkeList.subList(1, 2)); // [Cuba] 1 dahil 2 dahil degil
+        System.out.println("ulkeList sublist sonrasi = " + ulkeList); // [almanya, Cuba, Ingiltere]
 
 
 
         ArrayList<String>  hello = new ArrayList<>(ulkeList.subList(1,2)); // bos listeyi baska bir listeye atadik
-        System.out.println("hello = " + hello);
+        System.out.println("hello = " + hello); // Cuba
     }
 }
