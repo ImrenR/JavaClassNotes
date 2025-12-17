6db58548b1b9ed2b178c05e1df727a3202f2a79d
ImrenR <imrenrahbay@gmail.com>
2025-12-17 21:45:09 +0000

    tasks

--- src/j04_JavaOperators/j01_AritmeticOperators/tasks/_08_ArithmeticOperator_multiply1.java
+++ src/j04_JavaOperators/j01_AritmeticOperators/tasks/_08_ArithmeticOperator_multiply1.java
@@ -11,12 +11,17 @@ public class _08_ArithmeticOperator_multiply1 {
             Sonucu yazdırın.  */
 
         Scanner dp = new Scanner(System.in);
+        System.out.println("num1 sayisini giriniz :");
 
         double num1 = dp.nextDouble();
+        System.out.println("num2 sayisini giriniz :");
         double num2 = dp.nextDouble();
+        System.out.println("num3 sayisini giriniz :");
         double num3 = dp.nextDouble();
+
         double sonuc = num1*num2*num3;
-        System.out.println(sonuc);
+
+        System.out.println("sonuc = " + sonuc);
         //Kodu aşağıya yazınız.
 
 
