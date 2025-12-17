92476597fb0abf938d178a7899cce06f7594168d
ImrenR <imrenrahbay@gmail.com>
2025-12-17 21:48:46 +0000

    tasks

--- src/j04_JavaOperators/j01_AritmeticOperators/tasks/_10_Arithmetic_Operator_multiply3.java
+++ src/j04_JavaOperators/j01_AritmeticOperators/tasks/_10_Arithmetic_Operator_multiply3.java
@@ -11,11 +11,18 @@ public class _10_Arithmetic_Operator_multiply3 {
         int sonuc u yazdırın. */
 
         Scanner dp = new Scanner(System.in);
-
+        System.out.println("num1 sayisini giriniz:" );
         int num1 = dp.nextInt();
+        System.out.println("num2 sayisini giriniz:" );
         int num2 = dp.nextInt();
+        System.out.println("num3 sayisini giriniz:" );
         int num3 = dp.nextInt();
+        System.out.println("num4 sayisini giriniz:" );
         int num4 = dp.nextInt();
+
+        int sonuc = num1*num2*num3*num4;
+
+        System.out.println("sonuc = " + sonuc);
         //Kodu aşağıya yazınız.
 
 
