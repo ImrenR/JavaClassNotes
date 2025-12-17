7034dae9e4a4c3efb7797e74334cb9d1784b7e8f
ImrenR <imrenrahbay@gmail.com>
2025-12-17 18:02:17 +0000

    passByValue

--- src/j15_DateTime/C08_Period.java
+++ src/j15_DateTime/C08_Period.java
@@ -8,8 +8,7 @@ public class C08_Period {
     public static void main(String[] args) {
         // period-> iki tarih arasındaki geçen tekrarlana datayı tutar...
 
-        // Task-> 18 eylul 2023 başlayan java kursu 11 aylık planlanırsa
-        // bitiş tarihini gun sayısını print eden code create ediniz.
+
 
         LocalDate bugun = LocalDate.now();
         LocalDate imren = LocalDate.of(1991,1,14);
@@ -26,7 +25,36 @@ public class C08_Period {
         System.out.println("bugun.plus(p2) = " + bugun.plus(p2)); // bugunden itibaren 6 gunde 1 tekrarlanacak action
          // bir sonraki meeting 23 unde olacak gibi cunku bugun 17 si
 
+
+        // Task-> 18 eylul 2023 başlayan java kursu 11 aylık planlanırsa
+        // bitiş tarihini gun sayısını print eden code create ediniz.
+
+        LocalDate verilenGun = LocalDate.of(2023,9,18);
+         Period kurs = Period.ofMonths(11);
+//Kurs suresi iki bu sekilde de parcalayarak yazalibilir:
+
+        Period kurs2 = Period.ofYears(2023).ofMonths(9).ofDays(23);
+
+        System.out.println("kurs bitis = " + verilenGun.plus(kurs).format(DateTimeFormatter.ISO_LOCAL_DATE));
+       // kurs bitis 2024-08-18
+
+
+
+
+
+
+
+
+
+
+
         // ISO -> International Organization for Standardization
+
+
+
+
+
+
 
 
 
--- src/j16_PassByValue/C01_PassByValue.java
+++ src/j16_PassByValue/C01_PassByValue.java
@@ -1,17 +1,33 @@
 package j16_PassByValue;
 
+import java.time.Period;
+
 public class C01_PassByValue {
     public static void main(String[] args) {
                    /*
         Java Pass By Value bir programlama dilidir.
+
         Bir  primitive  variable  argument olarak bir methoda call edildiğinde variable
+
         değil bir copy clone (vesikalık) (pointer)değeri gönderilir.
 
          */
         // task -> verilen fiyat için %24 artırılmış fiyatı print eden METHOD  create ediniz.
 
+        Period kurs = Period.ofMonths(11);
+        double fiyat = 100; // double data type primiive variable create ettim.
+
+        System.out.println("Meth oncesi fiyat = " + fiyat); // 100
+        System.out.println("fiyatArttir(fiyat) = " + fiyatArttir(fiyat)); // 124
+        System.out.println("Method sonrasi fiyat = " + fiyat); // 100
 
     }// main sonu
+
+    private static double fiyatArttir(double a) {
+        return a*1.24;
+
+
+    }
 
 
 }//Class sonu
