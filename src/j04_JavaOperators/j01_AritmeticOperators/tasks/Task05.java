b8f1ef6b13eb106fb25f82b0d5d5b368ccb0550a
ImrenR <imrenrahbay@gmail.com>
2025-12-17 22:16:13 +0000

    tasks

--- src/j04_JavaOperators/j01_AritmeticOperators/tasks/Task05.java
+++ src/j04_JavaOperators/j01_AritmeticOperators/tasks/Task05.java
@@ -14,8 +14,20 @@ public class Task05 {
      */
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Saati giriniz : ");
+        int girilenSaat = input.nextInt();
+        System.out.println("Dakikayi giriniz :");
+        int girilenDk = input.nextInt();
+        System.out.println("sn yi giriniz :");
+        int girilenSn = input.nextInt();
 
+        int saat= 3600;
+        int dk = 60;
+       int sonuc = (girilenSaat*saat) + (girilenDk*dk) + girilenSn;
+
+        System.out.println("sonuc = " + sonuc);
     }
 
 }
