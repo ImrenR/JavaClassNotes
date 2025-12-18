e3e70379ddc098930bbf07797e6808557451f4dc
ImrenR <imrenrahbay@gmail.com>
2025-12-18 01:14:10 +0000

    tasks

--- src/j05_ifStatementTernaryOperator/tasks/Task06.java
+++ src/j05_ifStatementTernaryOperator/tasks/Task06.java
@@ -1,5 +1,6 @@
 package j05_ifStatementTernaryOperator.tasks;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Task06 {
@@ -12,8 +13,28 @@ public class Task06 {
 		Kadin ise yasini kontrol edin. Yaşı 18 den küçük ise ekrana “Kız çocuk” yazdırın.
 		Yaşı 18 den büyük eşit ise ekrana “Kadın” yazdırın.
 		 */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a gender:");
+        String gender= input.nextLine();
 
 
+        if (gender.equalsIgnoreCase("erkek")) {
+            Scanner db = new Scanner(System.in);
+            System.out.println("Yasini giriniz :");
+            int yasi = db.nextInt();
+            if (yasi <= 18) {
+                System.out.println("Erkek cocuk");
+            }else System.out.println("Adam");
+
+        }else if (gender.equalsIgnoreCase("kadin")) {
+            Scanner db = new Scanner(System.in);
+            System.out.println("Yasini giriniz :");
+            int yasi = db.nextInt();
+            if (yasi <= 18) {
+                System.out.println("Kiz cocuk");
+
+            }else System.out.println("Kadin");
+        }else System.out.println("something went wrong");
     }
 }
 
