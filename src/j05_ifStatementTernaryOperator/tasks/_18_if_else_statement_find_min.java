fe6d072a41693a8a72a51d11ff2a1a4cf1cb2be6
ImrenR <imrenrahbay@gmail.com>
2025-12-18 00:30:17 +0000

    tasks

--- src/j05_ifStatementTernaryOperator/tasks/_18_if_else_statement_find_min.java
+++ src/j05_ifStatementTernaryOperator/tasks/_18_if_else_statement_find_min.java
@@ -14,13 +14,20 @@ public class _18_if_else_statement_find_min {
 
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("i1 sayisini giriniz : ");
         int i1 = scan.nextInt();
+        System.out.println("i2 sayisini giriniz : ");
         int i2 = scan.nextInt();
+        System.out.println("i3 sayisini giriniz : ");
         int i3 = scan.nextInt();
 
         //Kodu aşağıya yazınız. BU SATIRIN YUKARISINI DEĞİŞTİRMEYİNİZ.
 
-
+        if (i3 < i2 && i3< i1) {
+            System.out.println("En kucuk numarayi buldunuz : i3");
+        } else if (i2 < i1 && i2 <i3) {
+            System.out.println("En kucuk numarayi buldunuz :i2 ");
+        }else System.out.println("En kucuk numanrayi buldunuz : i1");
 
 
     }
