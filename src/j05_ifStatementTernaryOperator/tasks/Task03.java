a599b9ab2b1c06ca3742d41bc997cba4970898c9
ImrenR <imrenrahbay@gmail.com>
2025-12-18 00:48:41 +0000

    tasks

--- src/j05_ifStatementTernaryOperator/tasks/Task03.java
+++ src/j05_ifStatementTernaryOperator/tasks/Task03.java
@@ -12,7 +12,17 @@ public class Task03 {
 		Eğer yıl 10’a bulunuyorsa ekrana “Onyıl" print eden code create ediniz.
 		 */
 
+Scanner input = new Scanner(System.in);
+        System.out.println("Bir yil giriniz :");
+        int yil = input.nextInt();
 
+        if (yil % 1000 == 0) {
+            System.out.println("Milenyum");
+        } else if (yil % 100 == 0) {
+            System.out.println("Yuzyil");
+        }else if (yil % 10 == 0) {
+            System.out.println("onyil");
+        }
     }
 }
 
