ceae0c464c3f0b83e203f616b86e70c0db6fd45a
ImrenR <imrenrahbay@gmail.com>
2025-12-18 01:27:48 +0000

    tasks

--- src/j05_ifStatementTernaryOperator/tasks/Task08.java
+++ src/j05_ifStatementTernaryOperator/tasks/Task08.java
@@ -12,7 +12,18 @@ Son rakamı 0 değil ise “5’e bölünen tek sayı” yazdırın.
 Girdiği password 5’e bölünmüyorsa ekrana “Tekrar deneyin” yazdırın.
 		 */
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        int password = input.nextInt();
 
+        if (password % 5 == 0) {
+            if (password %10 == 0) {
+                System.out.println(" 5 e bolunen cift sayi");
+
+            }else System.out.println("5 e bolunen tek sayi");
+
+
+        }else System.out.println("tekrar deneyin");
 	}
 
 }
