87d2102a432018737464156fbe02d253ba4d7926
ImrenR <imrenrahbay@gmail.com>
2025-12-19 21:53:39 +0000

    tasks

--- src/j11_Arrays/Tasks/_14_String_method_replace.java
+++ src/j11_Arrays/Tasks/_14_String_method_replace.java
@@ -10,6 +10,16 @@ public class _14_String_method_replace {
         Sonuç bu şekilde olmalıdır:
         HellK WKrld
          */
+String word = "Hello World";
+char [] chars = word.toCharArray(); //String i chara cevir ki tek tek karakterleri al
 
+        for (int i = 0; i <chars.length ; i++) {
+            if (chars[i] == 'o') {
+                chars[i] = 'K';
+            }
+
+        }
+String result = new String(chars);
+        System.out.println("result = " + result);
     }
 }
