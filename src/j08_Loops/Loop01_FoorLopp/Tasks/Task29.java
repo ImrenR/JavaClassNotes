c9d51ae479cc888153461514d6b24e8225f13e59
ImrenR <imrenrahbay@gmail.com>
2025-12-19 00:37:07 +0000

    tasks

--- src/j08_Loops/Loop01_FoorLopp/Tasks/Task29.java
+++ src/j08_Loops/Loop01_FoorLopp/Tasks/Task29.java
@@ -8,7 +8,23 @@ public class Task29 {
         // task-> girilen bir Stringde  index'i tek sayi olanlari kucuk harfle
         // index'i cift sayi olanlari buyuk harfle print eden code create ediniz
         // ornek : Haluk  output: HaLuK
+
+
         Scanner input=new Scanner(System.in);
+        System.out.println("Enter a string :");
+        String str = input.nextLine();
+
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (i %2 == 0){
+               result += Character.toUpperCase(ch);
+            }else {
+                result += Character.toLowerCase(ch);
+            }
+        }
+        System.out.println(result);
 
 
     }
