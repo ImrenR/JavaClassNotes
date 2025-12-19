58db2a1aeb8c028dfd767c1822c34a6e7139e2f6
ImrenR <imrenrahbay@gmail.com>
2025-12-19 19:40:21 +0000

    tasks

--- src/j10_MethodCreation/Tasks/Task09.java
+++ src/j10_MethodCreation/Tasks/Task09.java
@@ -10,9 +10,21 @@ public class Task09 {
    Task->  Saati saniyeye çeviren  method create ediniz
     */
         Scanner input = new Scanner(System.in);
+        System.out.println("Bir saat giriniz :");
+        int saat = input.nextInt();
+
+        int hesap = 0;
+
+       cevirSn(saat,hesap);
+
 
     }//main sonu
 
+    private static void cevirSn(int saat, int hesap) {
+
+        hesap += saat*3600;
+        System.out.println(hesap);
+    }
 
 
 }// Class sonu
