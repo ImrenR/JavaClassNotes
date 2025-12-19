977ac7a1f9848233e0c886b751e3bc5b7ebf40ff
ImrenR <imrenrahbay@gmail.com>
2025-12-19 20:52:47 +0000

    tasks

--- src/j10_MethodCreation/Tasks/Task12.java
+++ src/j10_MethodCreation/Tasks/Task12.java
@@ -3,6 +3,7 @@ package j10_MethodCreation.Tasks;
 import java.util.Scanner;
 
 public class Task12 {
+
     static Scanner input = new Scanner(System.in);//gunes eyledim ki herkeşler erişebile
 
     public static void main(String[] args) {
@@ -18,8 +19,24 @@ public class Task12 {
         "ebıkGabık@gmail.com"-> true
          */
 
+
+        System.out.println("Bir email giriniz :");
+        String mail = input.nextLine();
+
+        boolean result = mailKontrol(mail);
+        System.out.println(result);
+
     }//main sonu
 
+    private static boolean mailKontrol(String mail) {
+        if (!mail.contains("@") || !mail.contains(".")){
+            return false;
+        }
+        if (mail.indexOf("@") <1){
+            return false;
+        }
+        return true;
+    }
 
 
 }//Class sonu
