92e351765cee15ba339a3072798c5846a3d0a51b
ImrenR <imrenrahbay@gmail.com>
2025-12-19 20:59:57 +0000

    tasks

--- src/j10_MethodCreation/Tasks/Task13.java
+++ src/j10_MethodCreation/Tasks/Task13.java
@@ -1,11 +1,16 @@
 package j10_MethodCreation.Tasks;
 
+import java.util.Scanner;
+
 public class Task13 {
 
+
+static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
     /*
-    Task-> girilen iki String'den ikincisi, birincisinin son harflerine eşitse, true, diğer durumda false return eden METHOD create ediniz
+    Task-> girilen iki String'den ikincisi, birincisinin son harflerine eşitse, true,
+     diğer durumda false return eden METHOD create ediniz
 
     Örnekler:
    ("abc", "bc") ➞ true
@@ -14,13 +19,22 @@ public class Task13 {
    ("feminine", "nine") ➞ true
    ("convention", "tio") ➞ false
     */
+        System.out.println("Ilk stringi giriniz:");
 
-        String x = "feminine";
-        String y = "nine";
-        String z = "tio";
+        String x = input.nextLine();//"feminine";
+        System.out.println("ikinci string i giriniz: ");
+        String y = input.nextLine(); //"nine";
+        //String z = "tio";
 
+        System.out.println("sonHarfler esittir = " + sonHarfKontrol(x, y));
+//endsWith kullaniriz , true false dondurecek o zaman void yerine boolean yazariz
 
     }//main sonu
+
+    private static boolean sonHarfKontrol(String x, String y) {
+
+        return x.endsWith(y);
+    }
 
 
 }//Class sonu
