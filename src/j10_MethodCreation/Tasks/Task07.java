d5bd43d5d84c1f903b88410af69495934a53e745
ImrenR <imrenrahbay@gmail.com>
2025-12-19 19:23:12 +0000

    tasksofMethods

--- src/j10_MethodCreation/Tasks/Task07.java
+++ src/j10_MethodCreation/Tasks/Task07.java
@@ -8,10 +8,26 @@ public class Task07 {
     public static void main(String[] args) {
         //Task-> Girilen 3  sayıdan en küçüğünü print eden METHOD  create ediniz
 
+        System.out.println("Sayi1 yi giriniz :");
+        int sayi1 = input.nextInt();
+        System.out.println("Sayi2 yi giriniz :");
+        int sayi2= input.nextInt();
+        System.out.println("Sayi3 yi giriniz :");
+        int sayi3= input.nextInt();
 
+        int minSayi=0;
+        System.out.println("MaxSayi = " + kiyaslA(sayi1, sayi2, sayi3, minSayi));
 
     }//main sonu
 
+    private static int kiyaslA(int sayi1,int sayi2, int sayi3, int minSayi) {
+        if (sayi1 < sayi2 && sayi1 <sayi3) {
+            minSayi += sayi1;
+        } else if (sayi2<sayi1 && sayi2<sayi3) {
+            minSayi +=sayi2;
+        }else System.out.println("Max sayi sayi 3 tur");
+        return minSayi;
+    }
 
 
 }//Class sonu
