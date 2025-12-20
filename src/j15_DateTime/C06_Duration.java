e329cbe2ce666b81a8efbf693d9bb0becb86cefb
ImrenR <imrenrahbay@gmail.com>
2025-12-20 21:40:51 +0000

    duration

--- src/j15_DateTime/C06_Duration.java
+++ src/j15_DateTime/C06_Duration.java
@@ -6,13 +6,15 @@ import java.time.LocalTime;
 public class C06_Duration {
     public static void main(String[] args) {
 
-LocalTime gece = LocalTime.of(0,23);
-LocalTime gunduz = LocalTime.of(13,45);
 
-Duration fark1 = Duration.between(gece,gunduz);
-        System.out.println("fark1 = " + fark1); //13H22M
- Duration fark2= Duration.between(gunduz, gece);
-        System.out.println("fark2 = " + fark2); // -13H-22M
-        System.out.println("fark2.getSeconds() = " + fark2.getSeconds()); //  -48120
+  LocalTime gece =LocalTime.of(0,23);
+        LocalTime gunduz =LocalTime.of(13,45);
+
+        Duration fark1 =Duration.between(gece,gunduz);
+        Duration fark2 =Duration.between(gunduz,gece);
+        System.out.println("fark1 = " + fark1); // PT13H22M
+        System.out.println("fark2.getSeconds() = " + fark2.getSeconds());// PT13H22M
+        System.out.println("fark2 = " + fark2); // PT-13H-22M
+  // yazili tariuhleri , taksitler, odeme sureleri vs icin kullanilir
     }
 }
