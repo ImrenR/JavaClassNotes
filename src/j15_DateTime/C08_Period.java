dee880529b02dc5626cabe1290cb6063f35d002c
ImrenR <imrenrahbay@gmail.com>
2025-12-20 22:07:04 +0000

    period

--- src/j15_DateTime/C08_Period.java
+++ src/j15_DateTime/C08_Period.java
@@ -10,42 +10,32 @@ public class C08_Period {
 
 
 
-        LocalDate bugun = LocalDate.now();
+
+         LocalDate bugun =LocalDate.now(); // bugunu aldik
         LocalDate imren = LocalDate.of(1991,1,14);
-        Period p1 = Period.between(bugun,imren);
-        System.out.println("p1 = " + p1); // P-34Y-11M-3D
 
-        System.out.println("p1.getDays() = " + p1.getDays()); // -3
-        System.out.println("p1.getMonths() = " + p1.getMonths()); // -11
-        System.out.println("p1.getDays() = " + p1.getDays()); // -3
+        Period p1 =Period.between(bugun,imren); // bugun ile imren arasina bir period olusturdum
+        System.out.println("p1 = " + p1); // P-34Y-11M-6D
 
-        System.out.println("   *******************  " );
-
-        Period p2 = Period.ofDays(6); // 6 gunde 1 tekrarlanacak bir action gibi
-        System.out.println("bugun.plus(p2) = " + bugun.plus(p2)); // bugunden itibaren 6 gunde 1 tekrarlanacak action
-         // bir sonraki meeting 23 unde olacak gibi cunku bugun 17 si
+        System.out.println("p1.getDays() = " + p1.getDays()); // -6 (bugun 20 imrenin gunu 14-> 14-20= -6)
+        System.out.println("p1.getMonths() = " + p1.getMonths());  // -11 (12. ay imren 1. ay => 1-12= -11)
+        System.out.println("p1.getYears() = " + p1.getYears()); // -34
 
 
-        // Task-> 18 eylul 2023 başlayan java kursu 11 aylık planlanırsa
-        // bitiş tarihini gun sayısını print eden code create ediniz.
+        System.out.println("   *****    ");
 
-        LocalDate verilenGun = LocalDate.of(2023,9,18);
-         Period kurs = Period.ofMonths(11);
-//Kurs suresi iki bu sekilde de parcalayarak yazalibilir:
+        Period p2 = Period.ofDays(6); // her 6 gunde bir meeting olacak gibi
+        bugun.plus(p2); // yukarda days verdigimzi icin plusDays demeye gerek yok p2 zaten gundur
 
-        Period kurs2 = Period.ofYears(2023).ofMonths(9).ofDays(23);
+        System.out.println("bugun.plus(p2) = " + bugun.plus(p2)); // 2025-12-26 bir sonraki daily tarihi gibi mesela
 
-        System.out.println("kurs bitis = " + verilenGun.plus(kurs).format(DateTimeFormatter.ISO_LOCAL_DATE));
-       // kurs bitis 2024-08-18
+// Task-> 18 eylul 2023 başlayan java kursu 11 aylık planlanırsa bitiş tarihini gun sayısını print eden code create ediniz.
 
-
-
-
-
-
-
-
-
+        LocalDate kursStart = LocalDate.of(2023,9,18);
+        Period kursSuresi=Period.ofMonths(11);
+        System.out.println("kursStart = " + kursStart); // 2023-09-18
+        System.out.println("kursBitis" + kursStart.plus(kursSuresi).format(DateTimeFormatter.ISO_LOCAL_DATE));
+        // 2024-08-18
 
 
         // ISO -> International Organization for Standardization
