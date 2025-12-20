47d7693e5c3641dd4893be92e9967ad1f270135f
ImrenR <imrenrahbay@gmail.com>
2025-12-20 21:34:56 +0000

    ZonedDate

--- src/j15_DateTime/C05_ZoneIdDateTime.java
+++ src/j15_DateTime/C05_ZoneIdDateTime.java
@@ -6,20 +6,19 @@ public class C05_ZoneIdDateTime {
     public static void main(String[] args) {
         // herhangi bir ulke/sehir(local) tarih-zaman datasını tutar.
 
-        LocalDate usaDate =LocalDate.now(ZoneId.of("America/Los_Angeles"));
-        LocalTime usaTime = LocalTime.now(ZoneId.of("America/Los_Angeles"));
-        System.out.println("usaTime = " + usaTime);
-        System.out.println("usaDate = " + usaDate);
+       LocalDate usaDate= LocalDate.now(ZoneId.of("America/Los_Angeles"));
+        LocalTime usaTime= LocalTime.now(ZoneId.of("America/Los_Angeles"));
+        LocalDate japanDate= LocalDate.now(ZoneId.of("Japan"));
 
+        System.out.println("usaDate = " + usaDate); //  2025-12-20
+        System.out.println("usaTime = " + usaTime);  //  1v3:19:44.961210
+        System.out.println("japanDate = " + japanDate); // 2025-12-21
 
-       // ZonedDateTime zdt1= ZonedDateTime.now(ZoneId.of("Aruba"));
-        ZonedDateTime zdt2= ZonedDateTime.now(ZoneId.of("Japan"));
-      //  ZonedDateTime zdt3= ZonedDateTime.now(ZoneId.of("America")); // RTE => unknown time-zone id : America
-        ZonedDateTime zdt4= ZonedDateTime.now(ZoneId.of("CET"));
-        System.out.println("zdt4 = " + zdt4); // 2025-12-17T17:48:20.650701+01:00[CET]
-       // System.out.println("zdt1 = " + zdt1);
-        System.out.println("zdt2 = " + zdt2); // 2025-12-18T01:48:20.650454+09:00[Japan]
-     //   System.out.println("zdt3 = " + zdt3);
-
+       // ZonedDateTime zdt2 =ZonedDateTime.now(ZoneId.of("Japan")); // RTE-> Unknown time-zone
+       // ZonedDateTime zdt3 = ZonedDateTime.now(ZoneId.of("America")); //-> RTE
+        ZonedDateTime zdt4 = ZonedDateTime.now(ZoneId.of("CET")); //zdt4 = 2025-12-20T22:34:16.601619+01:00[CET]
+     //   System.out.println("zdt2 = " + zdt2);
+      //  System.out.println("zdt3 = " + zdt3);
+        System.out.println("zdt4 = " + zdt4);
     }
 }
