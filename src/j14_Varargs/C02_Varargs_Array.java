e6877c790f25459f2de580157bfbc623b71eebdb
ImrenR <imrenrahbay@gmail.com>
2025-12-20 17:25:08 +0000

    dateTime

--- src/j14_Varargs/C02_Varargs_Array.java
+++ src/j14_Varargs/C02_Varargs_Array.java
@@ -1,13 +1,18 @@
 package j14_Varargs;
 
 public class C02_Varargs_Array {
+
     public static void main(String[] args) {
+
         int arr[] = {10, 11, 36, 14, 41, 77, 46, 61, 20};
+
         // task01-> verilen arr elamanlarını toplamını print eden METHOD create ediniz.
 
         System.out.println("arrTopla(arr) = " + arrTopla(arr));
 
         System.out.println("varargsTopla(10, 11, 36, 14, 41, 77, 46, 61, 20) = " + varargsTopla(10, 11, 36, 14, 41, 77, 46, 61, 20));
+
+
         // task02-> task01 execute(run) eden varargs  METHOD create ediniz.
 // ahan da Trick köşesinde böyün -> varargs methoda girilen value'ler array elamanı olarak derlendiği için array parametre de alır...
 
--- src/j15_DateTime/C01_LoacalDate.java
+++ src/j15_DateTime/C01_LoacalDate.java
@@ -8,16 +8,21 @@ public class C01_LoacalDate {
         // LocalDate  -> sadece gun-ay-yıl datatlarını tutar(stored)
          // Trick-> LocalDate variable için new keyword kullanılmaz
 
-LocalDate bugun = LocalDate.now();
+   LocalDate bugun = LocalDate.now();
         System.out.println("bugun = " + bugun);
-        System.out.println("bugun.getDayOfMonth() = " + bugun.getDayOfMonth());
-        System.out.println("bugun.getDayOfWeek() = " + bugun.getDayOfWeek());
+        System.out.println("bugun.getDayOfMonth() = " + bugun.getDayOfMonth()); // 20 (raligin 20 si)
+        System.out.println("bugun.getDayOfWeek() = " + bugun.getDayOfWeek()); // saturday
+        System.out.println("bugun.getDayOfYear() = " + bugun.getDayOfYear()); // 354
+        System.out.println("bugun.getMonthValue() = " + bugun.getMonthValue()); // 12. ay
+        System.out.println("bugun.getMonth() = " + bugun.getMonth()); // DECEMBER
 
+        System.out.println("       **********         ");
 
-        System.out.println("**********");
         LocalDate falanTarih = LocalDate.of(1976,11,3);
-        //Tanimlanan tarih sonrasi icin -> plus. , oncesi icin minus() meth kullanilir..
-        System.out.println("falanTarih.plusYears(3).minusMonths(2).plusDays(23) = " + falanTarih.plusYears(3).minusMonths(2).plusDays(23));
+        // Tanimlanan tarih sonrasi icin --> plus()
+        // Tanimlanan tarih oncesi icin --> minus() meth kullanilir.
+
+        System.out.println("Plus and Minus date usage " + falanTarih.plusYears(3).minusMonths(2).plusDays(23)); // 1079-09-26
 
 
     }
