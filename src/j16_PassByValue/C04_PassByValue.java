4c84a053a88a292b2e4e68fef4cf89767d383278
ImrenR <imrenrahbay@gmail.com>
2025-12-21 22:44:48 +0000

    passByValue

--- src/j16_PassByValue/C04_PassByValue.java
+++ src/j16_PassByValue/C04_PassByValue.java
@@ -2,12 +2,12 @@ package j16_PassByValue;
 
 public class C04_PassByValue {//Class level
 
+  static double etiketFiyat; // gunes variable herkes erisebilir
+  static double indirimOrani;
+// Herkksin kullanacagi birseyse en tepeye static olarak yazariz
+  // => Class level static her gezegen(method) e erisimi olan initial (ilk deger atamasi)yapilmamis variable create edildi.
 
 
-  static   double etekietFiyat; // gunes variable herkes erisebilir
-    // => Class levelda her gezegen(methodlar) e erisimi olan inital (ilk deger atamasi yapilmamis)
-    // variable create edildi.
-    static double indirimOrani;
 
 
 
@@ -18,9 +18,17 @@ public class C04_PassByValue {//Class level
         eger bir method'a yapılan değişiklik kalıcı olsun istenirse
         1.değişiklik  yapılan variable'lar static (gökteki güneş) olarak tanımlanır.
          */
-
+etiketFiyat =100;
+indirimOrani =0.1;
+        System.out.println("Indirim oncesi etiketFiyat = " + etiketFiyat); // 100.0
+        indiirim(); // 90.00
+        System.out.println("Indirim sonrasi etiketFiyat = " + etiketFiyat); // 90.0
     }//main sonu
 
+public static void indiirim(){
+        etiketFiyat *=(1-indirimOrani);
+    System.out.println("indirilmis etiketFiyat = " + etiketFiyat); // 90
+}
 
 
 
--- src/j16_PassByValue/C05_PassByValue.java
+++ src/j16_PassByValue/C05_PassByValue.java
@@ -3,10 +3,18 @@ package j16_PassByValue;
 public class C05_PassByValue {
     public static void main(String[] args) {
           /*
-        eger bir method'a yapılan değişiklik kalıcı olsun istenirse
-       2 .değişklik  yapılan variable'lar atama tanımlanır.
+        eger bir method'a yapılan değişiklik kalıcı olsun istenirse  2 .değişklik  yapılan variable'lara atama tanımlanır.
          */
+        double etiketFiyat= 100; // primitive main level(scope) initial edilmis local variable
+        double indirimOrani= 0.1; // primitive main level(scope) initial edilmis local variable
 
+        System.out.println("Indirim oncesi EtiketFiyat = " + etiketFiyat); // 100
+        System.out.println("indirim(etiketFiyat,indirimOrani) = " + indirim(etiketFiyat, indirimOrani)); // 90
+        System.out.println("Indirim sonrasi etiketFiyat = " + etiketFiyat); // 100
+    }
+
+    static public double indirim(double fiyat,double oran){
+       return fiyat *= (1-oran); // burada yukardaki variablelarla alakali bir atama yok o yuzden son tahlilde etiketFiayti degismez
 
     }
 
