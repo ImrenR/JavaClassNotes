9180e3f3a3d5ba2de727bf6a63b028dc55e07ce5
ImrenR <imrenrahbay@gmail.com>
2025-12-25 17:45:13 +0000

    staticKeyword

--- src/j20_StaticKeyword/C02_StaticBlok.java
+++ src/j20_StaticKeyword/C02_StaticBlok.java
@@ -8,6 +8,29 @@ public class C02_StaticBlok {//class level
        4.static bloklar sadece 1 kere en basta calisir static olmayan bloklar her obje olusturulurken yeniden calisir
         */
 
+    static String isim="team lead  imren"; // static class variable
 
+    static { // ilk run edilecek static block
+        System.out.println("1.static block calisti");
+        System.out.println("1.static block update oncesi isim = " + isim);
+        isim ="developer imren";
+        System.out.println("1. static Update sonrasi isim =" + isim);
+    }
 
+    static {
+        System.out.println("2. static block calisti");
+    }
+    public static void main() { // main level
+        System.out.println(" main method basladi");
+        isim = "muhtesem haluk";
+        System.out.println("method update spnrasi " + isim);
+        System.out.println("main method bitti");
+
+    } // main sonu
+    static { // en son run edilecek static block
+        System.out.println("3.static block calisti");
+        System.out.println("3.static block update oncesi isim = " + isim);
+        isim ="fs developer imren";
+        System.out.println("3. static Update sonrasi isim =" + isim);
+    }
 }//Class sonu
