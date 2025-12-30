8cc0086219a3645af5466a1a33ee073d5053f3c3
ImrenR <imrenrahbay@gmail.com>
2025-12-30 21:51:49 +0000

    encapsulation

--- src/j23_Encapsulation/Tasks/task05/Book.java
+++ src/j23_Encapsulation/Tasks/task05/Book.java
@@ -4,25 +4,51 @@ import java.util.Scanner;
 
 public class Book {//outher Class level
     //fields...
-
+private  String bookName;
+private String authorName;
     //constructor...
+
+    public Book(String bookName, String authorName) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+    }
 
 
     //getter setter...
 
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+//toString...
 
 
-
-    //toString...
-
-
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", authorName='" + authorName + '\'' +
+                '}';
+    }
 }//outher class sonu
 
 class BookRunner {//inner Class level
 
     public static void main(String[] args) {
 
-
+Book bk=new Book("Avusturaylya Dinleri", "Eliade");
+        System.out.println("bk = " + bk);
     }
 }//inner class sonu
 
