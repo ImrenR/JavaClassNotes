b53bab19392a58a2816caf2a8e23a274b07a927e
ImrenR <imrenrahbay@gmail.com>
2025-12-30 18:11:56 +0000

    scope

--- src/j21_Scope/C01_InstanceVariable.java
+++ src/j21_Scope/C01_InstanceVariable.java
@@ -12,9 +12,9 @@ static String kurs= "Clarusway";
 
     int yas= 50; // initialized inst. variable
     int tecrube; // non -initialized inst variable
-    String name;
-    double boy;
-    char unvan;
+    String name;// non -initialized inst variable
+    double boy;// non -initialized inst variable
+    char unvan;// non -initialized inst variable
     boolean developerMi; //non-initialized inst. variable
 
 
@@ -34,43 +34,19 @@ static String kurs= "Clarusway";
                  char --> default value ' ''dir.
                  boolean --> default value false'dur.
                  String --> default value "null" dir.
-
-
-
  */
-        int a =31; // main levelda local create edilmis, local variable, main de acilmis cunku
-     //  static int a =31; => CTE error alir cunku static mainde not allowed der.
-        // static demek butun verilere ulasabilen deger demektir mainde kisitlanir erisilemez. Sadece class da kullanilir "Static"
-     int b; // initialize edilmemis local variable
-        // local i main blocktur
-      //  System.out.println("b = " + b); // CTE verir, eger initalize edilmezse sout edilmez, local de aksyion alamaz.
-        b= 17;
-        System.out.println("b = " + b); // local variable su an dogrudan cagirilabilir => // 17
-
-       // System.out.println("yas = " + yas); // initial edilmemis variable i cagiramazsin.obje olmadan call edilemez.
-
-        System.out.println("initialized static kurs = " + kurs); //initial static variable obj olmadan dogrudan call edilebilir.
-
-        System.out.println("non-initialized static programLanguage = " + programLanguage); // non initialize olan static default degerini atar.
-
-        static_method(); // static method dogrudan call yani galaksiler arasi astral seyahat
-     //   non_static_method(); static blok a (main) non static method dogrudan call edilemez.
-
-    C01_InstanceVariable obj= new C01_InstanceVariable(); // obj create edildi
-        obj.non_static_method(); // non-static method obj ile create edildi
-        System.out.println("obj.name = " + obj.name);
-        System.out.println("obj.boy = " + obj.boy);
-        System.out.println("obj.yas = " + obj.yas);
-        System.out.println("obj.developerMi = " + obj.developerMi);
-        System.out.println("obj.unvan = " + obj.unvan);
+       //static int a=31; => Class level haric LOCAL veya MAIN level da static CALL EDILEMEZ:CTE
+      int a =34; // => main level de local initialized var create edildi.
+      int b; // main level da local create edilmis non initialized variable
+      //System.out.println(b); // CTE verir cunku initialized yok
+      b=17;
+        System.out.println("b = " + b); // atama yaptiktan sonra call edeilebilir
+      //System.out.println("yas = " + yas);//  variable obj olmadan call edilemez : CTE
+        System.out.println("kurs = " + kurs); // initalized edilmis var obj olmadan call edilebilir cunku statictir.
+        System.out.println("programLanguage = " + programLanguage); // non-initial var initialzed olsun olmasin dogrudan call edilebilir : STATIC oldugu icin
+      // Static initalized and non initialized dogrudan call edilebilir main localde.
 
 
     }//main sonu
-       public static  void static_method(){ // galaksi-statik mehthof
-     System.out.println("Agama static-methodtan selamlar");
- }
 
-    public void non_static_method(){ // non-static
-        System.out.println("Agama non-static-methodtan selamlar");
-    }
 }//Class sonu
