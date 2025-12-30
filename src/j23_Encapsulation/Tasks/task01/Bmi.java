43e3748a2709b271a8bcaccf4ff81e663c7f05f7
ImrenR <imrenrahbay@gmail.com>
2025-12-30 20:46:30 +0000

    encapsulationTasks

--- src/j23_Encapsulation/Tasks/task01/Bmi.java
+++ src/j23_Encapsulation/Tasks/task01/Bmi.java
@@ -3,16 +3,42 @@ package j23_Encapsulation.Tasks.task01;
 public class Bmi {//pojo Class
     // fields -> name, age, weight,height: encapsulated
 
+    private  String name;
+    private  int age;
+    private  int weight;
+    private int height;
 
     //const.
 
-   // getter-setter
+    public Bmi(int age, String name, int weight, int height) {
+        this.age = age;
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public Bmi() {
+    }
+
+    // getter-setter
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+//toString..
 
 
-
-    //toString..
-
-
-
-
+    @Override
+    public String toString() {
+        return "Bmi{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", height=" + height +
+                '}';
+    }
 }//Class sonu
--- src/j23_Encapsulation/Tasks/task01/BmiRunner.java
+++ src/j23_Encapsulation/Tasks/task01/BmiRunner.java
@@ -3,6 +3,11 @@ package j23_Encapsulation.Tasks.task01;
 public class BmiRunner {
     public static void main(String[] args) {
 
+           Bmi obj=new Bmi(35,"imren",66,167);
 
+        System.out.println("obj.getWeight() = " + obj.getWeight()); // obj.getWeight() = 66
+        obj.setWeight(55);
+        System.out.println("obj = " + obj);
+        //obj = Bmi{name='imren', age=35, weight=55, height=167}
     }
 }
