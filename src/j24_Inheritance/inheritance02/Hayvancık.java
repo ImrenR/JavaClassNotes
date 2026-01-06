a719ce2c89c97ab4b4b8b6355faa4c312e274d21
ImrenR <imrenrahbay@gmail.com>
2026-01-06 09:47:32 +0000

    inheritance

--- src/j24_Inheritance/inheritance02/Hayvancık.java
+++ src/j24_Inheritance/inheritance02/Hayvancık.java
@@ -1,6 +1,14 @@
 package j24_Inheritance.inheritance02;
 
 public class Hayvancık {//Grand-parent (super) Class -> dede Class
+int a; // inst non initial var
+    int m=3;// inst initialized var
 
+    public Hayvancık(int x){
+        System.out.println("Agam ahan da Hayvancik pli const call...");
+    }
 
+    public Hayvancık() {
+        System.out.println("Psiz constructer");
+    }
 }//Class sonu
