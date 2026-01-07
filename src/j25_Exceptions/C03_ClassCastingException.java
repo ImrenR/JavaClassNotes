c9f1e41463e39c9df6e6b1f3605082d8838fe4cb
ImrenR <imrenrahbay@gmail.com>
2026-01-07 22:09:52 +0000

    ClassCastingException

--- src/j25_Exceptions/C03_ClassCastingException.java
+++ src/j25_Exceptions/C03_ClassCastingException.java
@@ -3,6 +3,29 @@ package j25_Exceptions;
 public class C03_ClassCastingException {
     public static void main(String[] args) {
         //ClassCastingException-> Biribirine donusturulemeyen data type'lar donusturme isleminde olusan RTE
+// bir objeyi, aslında ait olmadığı bir class türüne zorla çevirmeye (cast etmeye) çalıştığında oluşan RunTime Exception’dır.
+        //Yani : Bu nesne bu sınıftan değil ama ben öyleymiş gibi davranayım” dediğinde
+
+
+
+        Object obj = "Java Candir"; // String objesi
+
+        try {
+            Integer sayi =(Integer) obj;
+            System.out.println("sayi = " + sayi);
+        }catch (ClassCastException e){
+            System.out.println("ClassCastException yakalandi");
+            System.out.println("e.getMessage() = " + e.getMessage());
+
+        }
+
+        System.out.println("Program devam ediyor");
+        // ClassCastException yakalandi!
+        //java.lang.String cannot be cast to java.lang.Integer
+        //Program devam ediyor...
+
+
 
     }
+
 }
--- src/j25_Exceptions/C06_NumberFormatException.java
+++ src/j25_Exceptions/C06_NumberFormatException.java
@@ -2,7 +2,33 @@ package j25_Exceptions;
 
 public class C06_NumberFormatException {
     public static void main(String[] args) {
-        // NumberFormatException -> numerik formetta olmayan bir value parseInt() meth call edilirse oluşan RTE
+        // NumberFormatException -> numerik formatta olmayan bir value parseInt() meth call edilirse oluşan RTE
+
+        String imren="1903";
+        System.out.println(imren); //1903
+        System.out.println("imren + 34 = " + imren + 34);// 190334
+        //String i toplamak istersek int cevirmek zorundayiz
+
+        int nikola = Integer.parseInt(imren);
+        System.out.println("nikola = " + (nikola + 34)); // 1937
+
+        String id="386437564l";
+
+        try{
+            int invaId=Integer.parseInt(id);
+            System.out.println("invaId = " + invaId);
+            System.out.println("Sorunsuz calisti exception firlatmadi");
+
+        }catch (NumberFormatException e){
+            System.out.println(e.getMessage());
+            id="3864375641";
+            int invaId=Integer.parseInt(id);
+            System.out.println(invaId);
+            System.out.println("Print okunduysa try exception firlatti ve handle etti");
+        }
+//cikti =>
+//For input string: "386437564l"
+//Print okunduysa try exception firlatti ve handle etti
 
     }
 }
