1cbb49b1d8ee36baed0b3bb24204457c567c2be9
ImrenR <imrenrahbay@gmail.com>
2026-01-09 18:31:10 +0000

    abstract

--- src/j26_Abstract/abstarct01/Runner.java
+++ src/j26_Abstract/abstarct01/Runner.java
@@ -4,5 +4,34 @@ public class Runner {
 
     static void main() {
 
+        Civic c1=new Civic();
+        c1.name="Ahmet";
+        System.out.println("c1 = " + c1.name);
+        c1.motor();//1.6 motor neyine yetmez
+        c1.kapi();//kapi elinde kalmasin
+        c1.koltuk();//Koltuklar saglammmm he
+        c1.lastikEbat();// 21 inc lastik more than enough
+
+        System.out.println("       ************       ");
+        Accord a1=new Accord();
+        a1.name="Ayse";
+        System.out.println("a1.name = " + a1.name);
+        a1.motor();//
+        a1.kapi();//
+        a1.sunroof();//
+        a1.koltuk();//
+        System.out.println("       ************       ");
+
+        Crv r1=new Crv();
+        r1.name="Hayriye";
+        r1.kapi();//
+        r1.sunroof();//
+        r1.koltuk();//
+        r1.motor();//
+        System.out.println("r1.name = " + r1.name);
+
+
+        System.out.println("       ************       ");
+
     }
 }
