ce21426d53ee54a93e6c733e7ca730e211d046de
ImrenR <imrenrahbay@gmail.com>
2026-01-11 21:10:55 +0000

    tasks

--- src/j11_Arrays/Tasks/Task05.java
+++ src/j11_Arrays/Tasks/Task05.java
@@ -6,6 +6,9 @@ public class Task05 {
     public static void main(String[] args) {
         // Task-> girilen int değeri tersten print eden code create ediniz.
 
+
+
+
     }
 
 }
--- src/j11_Arrays/Tasks/Task08.java
+++ src/j11_Arrays/Tasks/Task08.java
@@ -7,5 +7,12 @@ public class Task08 {
     public static void main(String[] args) {
         //Task-> girilen 7 elemanlı bir int  arrayın tek elemanlarını print eden code create ediniz.
 
+        int [] arr = {2,3,5,6,8,10};
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]%2 !=0){
+                System.out.print( arr[i]+" ");
+            }
+        }
     }
 }
