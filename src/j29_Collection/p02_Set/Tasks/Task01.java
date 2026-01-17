63e52437e6487343a181e08cb96be9c5357251ff
ImrenR <imrenrahbay@gmail.com>
2026-01-17 19:10:08 +0000

    collectionTasks

--- src/j29_Collection/p02_Set/Tasks/Task01.java
+++ src/j29_Collection/p02_Set/Tasks/Task01.java
@@ -10,9 +10,29 @@ public class Task01 {
          */
         String arr[] = {"javaCAN", "javaSU", "javaNAZ", "javIYE", "javIDAN", "javaTAR", "javFER"};
 
+        // Method cagrisi =>
+
+            ArrayList<String> newArr=arrayToArrayList(arr);
+
+
+//Array list set donusumleri =>
+        HashSet<String> hash=new HashSet<>(newArr);
+        LinkedHashSet<String> linkedHash=new LinkedHashSet<>(newArr);
+        TreeSet<String> threeSet=new TreeSet<>(newArr);
+
+        System.out.println("hash = " + hash);
+        System.out.println("newArr = " + newArr);
+        System.out.println("linkedHash = " + linkedHash);
+        System.out.println("threeSet = " + threeSet);
+
+
+
 
     }//main sonu
+    //Array => Array Liste cevir
+    private static ArrayList<String> arrayToArrayList(String[] arr) {
+        return new ArrayList<>(Arrays.asList(arr)); // Arrayi liste cevirir.
+    }
 
 
-
- }
+}
