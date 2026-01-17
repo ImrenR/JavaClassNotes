10cd7e8fcb743ac79cb77e423a5d2ed0731933be
ImrenR <imrenrahbay@gmail.com>
2026-01-17 18:29:20 +0000

    iterators

--- src/j28_Iterators/Task03.java
+++ src/j28_Iterators/Task03.java
@@ -10,7 +10,14 @@ public class Task03 {
 
         ArrayList<Integer> sayiList = new ArrayList<>(Arrays.asList(14, 25, 36, 47, 58, 69, 12, 45, 78));
 
-
-
+// step 1 =>
+        ListIterator<Integer> newList= sayiList.listIterator();
+ // step 2 =>
+        while (newList.hasNext()){ // ilk elamandan sonra eleman var mi varsa while dongusunun icine gir
+            int num = newList.next(); //  her elemanin uzerinden gec son elemana kadar
+             num =num+3;
+            newList.set(num); // elemanlari listede guncelle
+        }
+        System.out.println("sayiList = " + sayiList); // sayiList = [17, 28, 39, 50, 61, 72, 15, 48, 81]
     }
 }
