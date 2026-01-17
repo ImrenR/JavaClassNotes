d21e8cdbe87fa8403d08a212ada0b43d492de838
ImrenR <imrenrahbay@gmail.com>
2026-01-17 05:54:36 +0000

    collections

--- src/j29_Collection/p01_LinkedList/C02_LinkedList.java
+++ src/j29_Collection/p01_LinkedList/C02_LinkedList.java
@@ -8,42 +8,13 @@ public class C02_LinkedList {
     public static void main(String[] args) {
 
 LinkedList<Object> ll1=new LinkedList<>(); //Data type object (Butun classlarin babasi)
-        //bad practice => app slows
+        //bad practice =>  slows the app
 
         ll1.add(31);
         ll1.add("imren");
         ll1.add('$');
         System.out.println("ll1 = " + ll1); //[1905,imren,$]
 
-        // Linked remove()=>
-        System.out.println("ll1.remove() = " + ll1.remove());
-        System.out.println("ll1 remove sonrasi= " + ll1);
-        System.out.println("ll1.remove(\"haluk\") = " + ll1.remove("haluk"));
-        System.out.println("ll1.remove(\"cuger\") = " + ll1.remove("cuger"));
-        System.out.println("ll1.removeFirst() = " + ll1.removeFirst());
-        System.out.println("ll1.removeLast() = " + ll1.removeLast());
-        System.out.println("ll1 removelar sonrasi = " + ll1);
-
-        LinkedList<String> ll2 =new LinkedList<>(Arrays.asList("havuc dilimi","dondurmali irmik","tirilece","kunefe","su muhallebisi", "hamsikoy muhallebisi"));
-        ll1.addAll(ll2);
-        System.out.println("ll1 add all ll2 hali = " + ll1);
-        System.out.println("ll1.removeAll(ll2) = " + ll1.removeAll(ll2));
-        System.out.println("ll1 = " + ll1);
-        System.out.println("ll2.element() = " + ll2.element()); // element ilk elemanini verir
-        // LinkedList get() getFirst() getLast() ->
-        System.out.println("ll2.get(3) = " + ll2.get(3));//
-        System.out.println("ll2.getFirst() = " + ll2.getFirst());
-        System.out.println("ll2.getLast() = " + ll2.getLast());
-
-        LinkedList<Integer> sayiList =new LinkedList<>(Arrays.asList(1,3,4,6,7,45,59,38));
-        System.out.println("sayiList = " + sayiList);
-        sayiList.remove(3); // 6
-        sayiList.remove(45); // CTE
-        // remove method parametre integer alirsa LinkedList  data type integer oldugu durumlarda meth parametre obj degil indexi derler.
-
-
-
-        // remove () meth parametre integer alirsa List data Type integer oldugunda meth parametre obj degil index tanimlanir.
 
 
     }
--- src/j29_Collection/p01_LinkedList/C03_LinkedList.java
+++ src/j29_Collection/p01_LinkedList/C03_LinkedList.java
@@ -5,15 +5,49 @@ import java.util.LinkedList;
 
 public class C03_LinkedList {
     public static void main(String[] args) {
+
         LinkedList<String> ll1 = new LinkedList<>(Arrays.asList("balcanKebap", "Cüger", "haşhaşKebap", "guzuGerdan", "incik", "küşşşleme"));
+
         System.out.println("ll1 ilk hali = " + ll1);// [balcanKebap, Cüger, haşhaşKebap, guzuGerdan, incik, küşşşleme]
 
-        // LinkedList remove()->
+
+        // Linked remove()=>
+
+        System.out.println("ll1.remove() = " + ll1.remove()); // balcanKebap ilk elemani sildi
+        System.out.println("ll1 remove sonrasi= " + ll1); //[Cüger, haşhaşKebap, guzuGerdan, incik, küşşşleme]
+        System.out.println("ll1.remove(\"haluk\") = " + ll1.remove("haluk")); // false haluk diye bir e yok
+        System.out.println("ll1.remove(\"cuger\") = " + ll1.remove("cuger")); // false case sensetive
+        System.out.println("ll1.removeFirst() = " + ll1.removeFirst()); // Cuger
+        System.out.println("ll1.removeLast() = " + ll1.removeLast()); // kusssleme
+        System.out.println("ll1 removelar sonrasi = " + ll1); //[haşhaşKebap, guzuGerdan, incik]
+
+
+        LinkedList<String> ll2 =new LinkedList<>(Arrays.asList("havuc dilimi","dondurmali irmik","tirilece","kunefe","su muhallebisi", "hamsikoy muhallebisi"));
+
+
+        ll1.addAll(ll2); // [haşhaşKebap, guzuGerdan, incik, havuc dilimi, dondurmali irmik, tirilece, kunefe, su muhallebisi, hamsikoy muhallebisi]
+        System.out.println("ll1 add all ll2 hali = " + ll1);
+        System.out.println("ll1.removeAll(ll2) = " + ll1.removeAll(ll2)); // true
+        System.out.println("ll1 = " + ll1); // [haşhaşKebap, guzuGerdan, incik]
+        System.out.println("ll2.element() = " + ll2.element()); // element ilk elemanini verir => havuc dilimi
 
         // LinkedList get() getFirst() getLast() ->
 
+        System.out.println("ll2.get(3) = " + ll2.get(3));//
+        System.out.println("ll2.getFirst() = " + ll2.getFirst());
+        System.out.println("ll2.getLast() = " + ll2.getLast());
 
-        // remove() meth parametre integer alırsa List data Type integer oldugunda meth parametre obj değil index tanımlar.
+        LinkedList<Integer> sayiList =new LinkedList<>(Arrays.asList(1,3,4,6,7,45,59,38));
+
+        System.out.println("sayiList = " + sayiList);
+        sayiList.remove(3); // 6
+        sayiList.remove(45); // CTE
+
+        // remove method parametre integer alirsa LinkedList  data type integer oldugu durumlarda meth parametre obj degil indexi derler.
+
+
+
+
 
     }
 }
