794f395fb7876c7cc7d4ae80de62c7cef817fcf5
ImrenR <imrenrahbay@gmail.com>
2026-01-18 16:45:43 +0000

    collection

--- src/j29_Collection/p02_Set/Tasks/Task01.java
+++ src/j29_Collection/p02_Set/Tasks/Task01.java
@@ -6,35 +6,35 @@ public class Task01 {
     public static void main(String[] args) {
        /* Task->
          Deger ataması yapılan Array'i arraylist'e ceviren method create ediniz
-         main içersinde meth return ettiği arryList'i hashSet, linkedHashSet ve TreeSett'e atayınız.
+         main içerisinde meth return ettiği arryList'i
+          hashSet,
+           linkedHashSet ve
+           TreeSett'e atayınız.
          */
+
         String arr[] = {"javaCAN", "javaSU", "javaNAZ", "javIYE", "javIDAN", "javaTAR", "javFER"};
 
         // Method cagrisi =>
+       ArrayList<String> newArr= arrayToList(arr);
 
-            ArrayList<String> newArr=arrayToArrayList(arr);
+      HashSet<String> hashSet = new HashSet<>(newArr);
+      LinkedList<String> linkedList=new LinkedList<>(newArr);
+      TreeSet<String> treeSet=new TreeSet<>(newArr);
 
+        System.out.println("newArr = " + newArr); //[javaCAN, javaSU, javaNAZ, javIYE, javIDAN, javaTAR, javFER]
+        System.out.println("hashSet = " + hashSet); //[javaNAZ, javaTAR, javaCAN, javIYE, javaSU, javFER, javIDAN]
+        System.out.println("linkedList = " + linkedList);// [javaCAN, javaSU, javaNAZ, javIYE, javIDAN, javaTAR, javFER]
+        System.out.println("treeSet = " + treeSet);// [javFER, javIDAN, javIYE, javaCAN, javaNAZ, javaSU, javaTAR]
+ // IMPORTANT NOTES
+        // 1- Array lst = Sirali , tekrar olabilir
+        // 2- HashSet   = Sira yok , tekrar YOK =>> EN HIZLI
+        // 3- LinkedHashSet= Ekleme sirasini korur, tekrar yok.
+        //4- TreeSet =    ALfabetik siralar, tekrar yok, elamanlar comparable olmali ( Neden yavas, cunku her eklemede siralama yapar)
 
-//Array list set donusumleri =>
-        HashSet<String> hash=new HashSet<>(newArr);
-        LinkedHashSet<String> linkedHash=new LinkedHashSet<>(newArr);
-        TreeSet<String> threeSet=new TreeSet<>(newArr);
+    }
 
-        System.out.println("hash = " + hash);
-        System.out.println("newArr = " + newArr);
-        System.out.println("linkedHash = " + linkedHash);
-        System.out.println("threeSet = " + threeSet);
-
-
-
-
-    }//main sonu
-    //Array => Array Liste cevir
-
-                     //DataType       //MethodName      //Array elemanlari
-    private static ArrayList<String> arrayToArrayList(String[] arr) {
-
-        return new ArrayList<>(Arrays.asList(arr)); //=> Arrayi liste cevirir.
+    private static ArrayList<String> arrayToList(String[] arr) {
+return new ArrayList<>(Arrays.asList(arr)); // array i liste ceviren Arrays.asList tir
     }
 
 
