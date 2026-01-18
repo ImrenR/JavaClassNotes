6c4db0b56b0dac272c316c6ca2114f921c7e72f4
ImrenR <imrenrahbay@gmail.com>
2026-01-18 22:35:31 +0000

    CollectionTask03

--- src/j08_Loops/Loop02_WhileLoop/Tasks/Task03.java
+++ src/j08_Loops/Loop02_WhileLoop/Tasks/Task03.java
@@ -7,5 +7,14 @@ public class Task03 {
         //task-> girilen tamsayı için çarpım tablosu print eden code create ediniz
         //input: 8-> output :8x1=8, 8x2=16... 8x10=80
 
+        Scanner input=new Scanner(System.in);
+        System.out.println("Bir sayi giriniz: ");
+        int sayi= input.nextInt();;
+
+        for (int i = 1; i <=10; i++) {
+            System.out.println(sayi + "x" + i + "=" + (sayi*i));
+
+
+        }
     }
 }
