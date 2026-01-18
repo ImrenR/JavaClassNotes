c15a9f1c7322244971d0c888aa6149cdb200592c
ImrenR <imrenrahbay@gmail.com>
2026-01-18 17:42:54 +0000

    collectionTask07

--- src/j29_Collection/p02_Set/Tasks/Task06.java
+++ src/j29_Collection/p02_Set/Tasks/Task06.java
@@ -5,18 +5,16 @@ import java.util.HashSet;
 
 public class Task06 {
     public static void main(String[] args) {
-// Task 1 : bir metod oluşturup bir HashSet'i verilen bu numaralar ile doldurunuz [1, 2, 3, 4, 5, 5, 5, 6, 7, 8]
-// bu işlemi generateSet() metodunda yapınız.mainden yazdırınız.
+// Task 1 : bir metod oluşturup bir HashSet'i verilen bu numaralar ile doldurunuz
+// [1, 2, 3, 4, 5, 5, 5, 6, 7, 8]
+// bu işlemi generateSet() metodunda yapınız ve
+// mainden yazdırınız.
 
+// AddElements şeklinde bir metodla 1 veya 1 den fazla sayı eklenebilen metodu  yazınız.
 
-
-
-        // AddElements şeklinde bir metodla 1 veya 1 den fazla sayı eklenebilen metodu  yazınız.
-
-
-HashSet<Integer> hashSet = generateSet();
+       HashSet<Integer> hashSet = generateSet();
         System.out.println("hashSet = " + hashSet);//hashSet = [1, 2, 3, 4, 5, 6, 7, 8]
-addElements(hashSet,10,11,12,13);
+       addElements(hashSet,10,11,12,13);
         System.out.println("hashSet = " + hashSet); // hashSet = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13]
     }
 
--- src/j29_Collection/p02_Set/Tasks/Task07.java
+++ src/j29_Collection/p02_Set/Tasks/Task07.java
@@ -2,6 +2,7 @@ package j29_Collection.p02_Set.Tasks;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 
 public class Task07 {
 
@@ -20,5 +21,22 @@ public class Task07 {
 
     public static void main(String[] args) {
 
+        LinkedList<String> linkedList = new LinkedList<>(Arrays.asList("Germany","France","USA","Canada","Mexico","Brazil"));
+
+        String str1= "Germany";
+        String str2= "USA";
+
+        linkedList=removing(linkedList,str2,str1);
+        System.out.println("linkedList = " + linkedList); // linkedList = [France, Canada, Mexico, Brazil]
+
+    }
+
+    private static LinkedList<String> removing(LinkedList<String> linkedList, String str2, String str1) {
+   if (linkedList.contains(str1) || linkedList.contains(str2)){
+       linkedList.remove(str1);
+       linkedList.remove(str2);
+
+   }
+   return linkedList;
     }
 }
