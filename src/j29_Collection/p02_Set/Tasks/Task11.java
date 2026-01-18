5b3aede6152157d73bd7f99e10d681bfce83d28a
ImrenR <imrenrahbay@gmail.com>
2026-01-18 21:04:03 +0000

    whileLoopTask1

--- src/j29_Collection/p02_Set/Tasks/Task11.java
+++ src/j29_Collection/p02_Set/Tasks/Task11.java
@@ -12,6 +12,32 @@ public class Task11 {
         // Task 2 : Bu SET i bir fonksiyonda diziye çevirinizve yazdırınız.
 
 
+
+        HashSet<Integer> liste=new HashSet<>();
+        Random rnd=new Random();
+
+         while(liste.size() <10){
+             liste.add(rnd.nextInt(20)+1);
+         }
+
+        System.out.println("liste = " + liste);
+        System.out.println("liste.size() = " + liste.size());
+
+
+// set i Array e cevir ve yazdir
+        Integer[] array=convertToArray(liste);
+        System.out.println("Array version: ");
+        for(Integer num:array){
+            System.out.print(num+ " ");
+        }
+    }
+
+    // seti array e ceviren metod
+
+             // metodun donus tipi bura: Integer[]       //Metodun parametresi,yani cevirmek istedigin set = HashSet
+    private static Integer[] convertToArray(HashSet<Integer> liste) {
+
+        return liste.toArray(new Integer[0]);
     }
 
 }
