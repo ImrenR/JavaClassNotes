3b2a1eaffd6d4e452154749b3408198291402a0b
ImrenR <imrenrahbay@gmail.com>
2026-01-18 17:35:52 +0000

    collectionTask6

--- src/j29_Collection/p02_Set/Tasks/Task06.java
+++ src/j29_Collection/p02_Set/Tasks/Task06.java
@@ -5,9 +5,8 @@ import java.util.HashSet;
 
 public class Task06 {
     public static void main(String[] args) {
-        // Task 1 : bir metod oluşturup bir HashSet i
-        // verilen bu numaralar ile doldurunuz [1, 2, 3, 4, 5, 5, 5, 6, 7, 8]
-        // bu işlemi generateSet() metodunda yapınız.mainden yazdırınız.
+// Task 1 : bir metod oluşturup bir HashSet'i verilen bu numaralar ile doldurunuz [1, 2, 3, 4, 5, 5, 5, 6, 7, 8]
+// bu işlemi generateSet() metodunda yapınız.mainden yazdırınız.
 
 
 
@@ -15,8 +14,19 @@ public class Task06 {
         // AddElements şeklinde bir metodla 1 veya 1 den fazla sayı eklenebilen metodu  yazınız.
 
 
-
-
-
+HashSet<Integer> hashSet = generateSet();
+        System.out.println("hashSet = " + hashSet);//hashSet = [1, 2, 3, 4, 5, 6, 7, 8]
+addElements(hashSet,10,11,12,13);
+        System.out.println("hashSet = " + hashSet); // hashSet = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13]
     }
+
+    private static void addElements(HashSet<Integer> hashSet, Integer... numbers) {
+    hashSet.addAll(Arrays.asList(numbers));
+    }
+
+    private static HashSet<Integer> generateSet() {
+        return new HashSet<>(Arrays.asList(1,2,3,4,5,5,5,6,7,8));
+    }
+
+
 }
