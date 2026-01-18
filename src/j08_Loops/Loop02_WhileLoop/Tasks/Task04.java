c6287d4ff4c9188529681ff64ad6d6d3815cdecb
ImrenR <imrenrahbay@gmail.com>
2026-01-18 23:01:52 +0000

    task05Collection

--- src/j08_Loops/Loop02_WhileLoop/Tasks/Task04.java
+++ src/j08_Loops/Loop02_WhileLoop/Tasks/Task04.java
@@ -1,5 +1,7 @@
 package j08_Loops.Loop02_WhileLoop.Tasks;
 
+import java.util.Scanner;
+
 public class Task04 {
 
     public static void main(String[] args) {
@@ -7,6 +9,18 @@ public class Task04 {
 		task->  3 basamakli 4 ve 6 ya tam bolunebilen tam sayilari buyukten kucuge ve kaç tane oldugunu print eden code create ediniz.
 
 		 */
+        int sayi=999;
+        int sayac=0;
+
+       while (sayi>=100) {
+           if(sayi % 12 == 0){
+               System.out.print( sayi + " ");
+               sayac++;
+           }
+           sayi--; //kucuge dogru ilerle
+       }
+        System.out.println();
+        System.out.println("sayac = " + sayac);
 
     }
 
--- src/j08_Loops/Loop02_WhileLoop/Tasks/Task05.java
+++ src/j08_Loops/Loop02_WhileLoop/Tasks/Task05.java
@@ -6,10 +6,21 @@ public class Task05 {
 
 	public static void main(String[] args) {
 		/*
-		task-> girilen  isim ve soyismi için ilk isminin ilk harfinden soy isminin son harfine kadar olan butun harfleri
+		task-> girilen  isim ve soyismi için ilk isminin ilk harfinden soy isminin
+		son harfine kadar olan butun harfleri
 		 ayni satirda aralarina bosluk birakarak print rden code create ediniz
 
 		 */
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Ismi giriniz :");
+        String isimSoyisim= input.nextLine();
+
+        for (int i = 0; i <isimSoyisim.length(); i++) {
+            System.out.print( isimSoyisim.charAt(i) + " ");
+
+
+        }
 
 
 	}
