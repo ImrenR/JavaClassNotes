1675135f0c2d4e7671101452c8581b48714183be
ImrenR <imrenrahbay@gmail.com>
2026-01-19 08:02:33 +0000

    Task03WhileLopps

--- src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task02.java
+++ src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task02.java
@@ -5,10 +5,25 @@ import java.util.Scanner;
 public class Task02 {
 
     public static void main(String[] args) {
+
         //Task-> girilen baslangic ve bitis karakterleri dahil
         // aradaki tum karakterleri pirnt eden code create ediniz
+
+
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Baslangic karakterini giriniz : ");
+        char start = input.next().charAt(0);
+
+        System.out.println("Bitis karakterini giriniz : ");
+        char end = input.next().charAt(0);
+
+        char ch=start;
+
+        do {
+            System.out.print(ch+ " ");
+            ch++;
+        }while (ch<=end);
 
     }
 }
--- src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task03.java
+++ src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task03.java
@@ -1,5 +1,7 @@
 package j08_Loops.Loop03_DoWhileLoop.Tasks;
 
+import java.util.Scanner;
+
 public class Task03 {
 
     public static void main(String[] args) { /*
@@ -8,10 +10,25 @@ public class Task03 {
      Ornek :  input : 16, output: 4 */
 
 
+        Scanner input=new Scanner(System.in);
+        System.out.println("Bir tam sayi giriniz :");
+         int sayi;
+
+        do {
+            sayi=input.nextInt();
 
 
+            }while (sayi<=0); // negatif veya 0 ise tekrar sor
+
+        double karekok = Math.sqrt(sayi);
+
+        if(karekok ==(int) karekok) {
+            System.out.println("Bu sayi bir tam karedir =" + (int) karekok);
+        }else
+            System.out.println("Bu sayi tam kare degildir");
+        }
 
 
 
     }
-}
+
