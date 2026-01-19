fcf8152b58f6850eedb15c88296ec0978806977e
ImrenR <imrenrahbay@gmail.com>
2026-01-19 06:16:59 +0000

    Task11WhileLoops

--- src/j08_Loops/Loop02_WhileLoop/Tasks/Task11.java
+++ src/j08_Loops/Loop02_WhileLoop/Tasks/Task11.java
@@ -8,11 +8,24 @@ public class Task11 {
     task -> 0 (sıfır) girilene kadar girilen tum sayıların adedini ve toplamını print eden code create edinz.
     */
 
+Scanner input = new Scanner(System.in);
 
+       int sayac=0;
+       int toplam=0;
+        System.out.println("Sayı giriniz (0 girerseniz çıkış yapar):");
+        int sayi = input.nextInt();
 
+      while(sayi !=0){
+      toplam+=sayi;
+      sayac++;
 
+    System.out.println("sayi giriniz :");
+    sayi = input.nextInt();
 
+}
 
+        System.out.println("Girilen sayi adedi = " + sayac);
+        System.out.println("girilen sayilarin toplami = " + toplam);
 
     }
 }
