5d5fabfd1dc5db85df0046c58f6a7bec0815d788
ImrenR <imrenrahbay@gmail.com>
2026-01-19 06:04:55 +0000

    Task10WhileLoops

--- src/j08_Loops/Loop02_WhileLoop/Tasks/Task10.java
+++ src/j08_Loops/Loop02_WhileLoop/Tasks/Task10.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class Task10 {
     public static void main(String[] args) {
         // task ->  Girilen bir sayıya kadar  olan tek sayıları print eden code create ediniz.
+// 5 giriliyorsa 5 ten 1 e kadar gibi
 
+
+        Scanner input =new Scanner(System.in);
+     System.out.println("Bi sayiyi giriniz : ");
+        int sayi= input.nextInt();
+       int i =1;
+        while(i<=sayi){
+
+            if(i %2 !=0){
+                System.out.print( i + " ");
+
+            }
+            i++;
+        }
 
     }
 }
