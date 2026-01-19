2368e7b68978ce1782ecd2e982d6274bfc2905bb
ImrenR <imrenrahbay@gmail.com>
2026-01-19 08:25:25 +0000

    Task04WhileLoops

--- src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task04.java
+++ src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task04.java
@@ -1,5 +1,7 @@
 package j08_Loops.Loop03_DoWhileLoop.Tasks;
 
+import java.util.Scanner;
+
 public class Task04 {
     public static void main(String[] args) {
 
@@ -7,5 +9,18 @@ public class Task04 {
         //             24.5673 ==>    5+6+7+3 = 21
 
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ondalik bir sayi giriniz :");
+        double sayi=input.nextDouble();
+
+        String sayiStr = String.valueOf(sayi); // sayiyi stringe cevir
+        int noktaIndex = sayiStr.indexOf('.'); // noktanin indexi
+        int toplam=0;
+
+        for (int i = noktaIndex; i < sayiStr.length(); i++) {
+            toplam  += Character.getNumericValue(sayiStr.charAt(i));
+
+        }
+        System.out.println("toplam = " + toplam);
     }
 }
