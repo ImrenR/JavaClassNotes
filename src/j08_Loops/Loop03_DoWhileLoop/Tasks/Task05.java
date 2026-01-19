927cb22e53c09bfd3f699898887ca2213b0d618f
ImrenR <imrenrahbay@gmail.com>
2026-01-19 08:36:47 +0000

    Task05WhileLopps

--- src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task05.java
+++ src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task05.java
@@ -7,7 +7,15 @@ public class Task05 {
 
         //task-> Girilen ismin ilk harfinin buyuk olmasını kontrol eden code create ediniz.
 
+Scanner input = new Scanner(System.in);
 
+        String isim;
 
+        do {
+            System.out.println("Bir isim giriniz :");
+         isim = input.nextLine();
+            System.out.println("ismin ilk harfi buyuk degil");
+        }while (!Character.isUpperCase(isim.charAt(0)));
+        System.out.println("ismin ilk harfi buyuk");
     }
 }
