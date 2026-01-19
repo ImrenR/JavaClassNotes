2cc3ae061fdeaf497eb7eb1c9018f7cf89301143
ImrenR <imrenrahbay@gmail.com>
2026-01-19 05:17:07 +0000

    task06WhileLoops

--- src/j08_Loops/Loop02_WhileLoop/Tasks/Task06.java
+++ src/j08_Loops/Loop02_WhileLoop/Tasks/Task06.java
@@ -7,11 +7,32 @@ public class Task06 {
 
     public static void main(String[] args) {
         /*
-        1 ile 100    sayi arasındaki  pc'nin atadığı tamsayıyı kullanıcıya  tahmin ettirip tahmin sayısını print eden  code create ediniz.
+        1 ile 100    sayi arasındaki  pc'nin atadığı tamsayıyı
+        kullanıcıya tahmin ettirip
+        tahmin sayısını print eden  code create ediniz.
          */
 
+        Scanner input=new Scanner(System.in);
+        Random sayi=new Random();
 
+        int pcSayisi= sayi.nextInt(100) +1; // 1-100 arasinda bir sayi verecek pc
+        int tahmin = 0; // benim tahminim
+        int tahminSayisi=0;
 
+        while(tahmin != pcSayisi){
+            System.out.println("tahmini gir :");
+            tahmin=input.nextInt();
+            tahminSayisi++;
+
+            if(tahmin>pcSayisi){
+                System.out.println("Daha kucuk bir sayi giriniz");
+            } else if (tahmin<pcSayisi) {
+                System.out.println("Daha buyuk bbir sayi giriniz");
+            }
+        }
+
+        System.out.println("tahminSayisi = " + tahminSayisi);
     }
+
 
 }
--- src/j08_Loops/Loop02_WhileLoop/Tasks/Task07.java
+++ src/j08_Loops/Loop02_WhileLoop/Tasks/Task07.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 
 public class Task07 {
     public static void main(String[] args) {
+
+
         // Task-> girilen 5 sayının en buyugunu print eden code create ediniz
+
+
         Scanner input = new Scanner(System.in);
+
 
     }
 }
