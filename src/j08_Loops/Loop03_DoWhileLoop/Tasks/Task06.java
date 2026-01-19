881e1644545cc1044995c1799508da121b9a800c
ImrenR <imrenrahbay@gmail.com>
2026-01-19 08:53:32 +0000

    Task06WhileLoops

--- src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task06.java
+++ src/j08_Loops/Loop03_DoWhileLoop/Tasks/Task06.java
@@ -4,7 +4,28 @@ import java.util.Scanner;
 
 public class Task06 {
     public static void main(String[] args) {
-        //task-> kullanıcıdan alınan bir cümlenin büyük harfle başlayıp nokta ile bittiğini kontrol eden code create ediniz.
 
+        //task-> kullanıcıdan alınan
+        // bir cümlenin büyük harfle başlayıp nokta ile
+        // bittiğini kontrol eden code create ediniz.
+// COZUM icin puf noktalar =>>
+        /*
+        * cumle.chart(0) => cumlenin ilk harfi
+        * Character.isUpperCase(...) => buyuk mu kucuk mu
+        * cumle.charAt(cumle.lenght()-1) == '.' => cumlenin sonu . ile bitiyor mu
+        *
+        *
+        * */
+        Scanner input = new Scanner(System.in);
+
+        String cumle;
+
+        do {
+            System.out.println("Bir cumle giriniz :");
+            cumle= input.nextLine();
+
+
+        }while (!Character.isUpperCase(cumle.charAt(0)) || cumle.charAt(cumle.length()-1) != '.');
+        System.out.println("Cumle kurallara uygun");
     }
 }
