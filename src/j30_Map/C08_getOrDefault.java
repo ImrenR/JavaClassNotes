9245c9a1afe8558a9f87777d09dcf2eb4be4cead
ImrenR <imrenrahbay@gmail.com>
2026-01-19 16:57:55 +0000

    map

--- src/j30_Map/C08_getOrDefault.java
+++ src/j30_Map/C08_getOrDefault.java
@@ -4,7 +4,18 @@ import java.util.HashMap;
 
 public class C08_getOrDefault {
     public static void main(String[] args) {
+
         // map.getOrDefault(); -> girilen key map'de varsa key'e ait value yoksa default değer return eder
 
+        HashMap<String, String> hm = new HashMap<>();//bos hm tanımlandı
+        hm.put("Amazon", "296 Euro");
+        hm.put("Saturn", "200 Euro");
+        hm.put("Vatan", "111 Euro");
+        hm.put("Apple", "450 Euro");
+        hm.put("Teknosa", "333 Euro");
+        hm.put("Media Markt", "444 Euro");
+        System.out.println("hm getOrDefault oncesi= " + hm);
+
+        System.out.println("hm.getOrDefault(\"Apple\",\"javaSu\") = " + hm.getOrDefault("Apple", "javaSu"));
     }
 }
