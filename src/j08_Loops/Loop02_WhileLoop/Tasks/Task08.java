c868b894c82c1b33bf51cff75c1ac5b352c12649
ImrenR <imrenrahbay@gmail.com>
2026-01-19 05:50:07 +0000

    Task08WhileLoops

--- src/j08_Loops/Loop02_WhileLoop/Tasks/Task08.java
+++ src/j08_Loops/Loop02_WhileLoop/Tasks/Task08.java
@@ -1,10 +1,12 @@
 package j08_Loops.Loop02_WhileLoop.Tasks;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Task08 {
     public static void main(String[] args) {
-        // task-> girilen şifrenin asagidaki sartlari kontrol eden code create ediniz.
+        // task->
+        // girilen şifrenin asagidaki sartlari kontrol eden code create ediniz.
         // ve kullaniciya duzeltmesi gereken tum eksikleri soyleyin,
         // eger tum sartlari saglarsa, "sifre basariyla kaydedildi" print edin
         // - ilk harf kucuk harf olmali
@@ -13,11 +15,33 @@ public class Task08 {
         // - uzunlugu en az 10 karakter olmali
 
 
+        System.out.println("Enter your password :");
+        Scanner input = new Scanner(System.in);
+        String sifre= input.nextLine();
 
 
+        boolean gecerliMi = true;
 
+        if(sifre.length()<10){
+            System.out.println("Sifre en az 10 karakter olmali");
+            gecerliMi=false;
+        }
 
+if(!Character.isLowerCase(sifre.charAt(0))){
+    System.out.println("Ilk harf kucuk olmali!");
+    gecerliMi= false;
+}
 
-
+if(!Character.isDigit(sifre.charAt(sifre.length()-1))){
+    System.out.println("son karakter rakam olmali");
+    gecerliMi=false;
+}
+if(sifre.contains(" ")){
+    System.out.println("Sifre bosluk icermemeli");
+    gecerliMi=false;
+}
+if(gecerliMi){
+    System.out.println("Sifre basariyla kaydedildi.");
+}
     }
 }
