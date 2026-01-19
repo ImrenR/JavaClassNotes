2483959ca0aeb8271193d0f294506ff3ca7f760c
ImrenR <imrenrahbay@gmail.com>
2026-01-19 06:42:27 +0000

    Task13WhileLopps

--- src/j08_Loops/Loop02_WhileLoop/Tasks/Task13.java
+++ src/j08_Loops/Loop02_WhileLoop/Tasks/Task13.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 public class Task13 {
     public static void main(String[] args) {
         /*
-        task -> x karakteri girilene kadar "javaCAN" x karakteri girildiğinde "javaTAR" print eden code create ediniz.
+        task -> x karakteri girilene kadar "javaCAN"
+        x karakteri girildiğinde "javaTAR" print eden code create ediniz.
         do-while lopp
        */
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("x karakteri girilene kadar devamke");
+char ch;
 
+        do {
+            ch=input.next().charAt(0);
+
+            if(ch == 'x'){
+                System.out.println("javaTAR");
+            }else System.out.println("javaCAN");
+        }while (ch != 'x'); // x girilene kadar devam !
     }
 }
