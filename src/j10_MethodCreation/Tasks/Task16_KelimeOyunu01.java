61aa7009eb27611256750b50fcc51629496b0645
ImrenR <imrenrahbay@gmail.com>
2026-01-30 16:55:45 +0000

    OCAtasks

--- src/j10_MethodCreation/Tasks/Task16_KelimeOyunu01.java
+++ src/j10_MethodCreation/Tasks/Task16_KelimeOyunu01.java
@@ -1,11 +1,13 @@
 package j10_MethodCreation.Tasks;
 
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 
 public class Task16_KelimeOyunu01 {
 
+    static  Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -28,7 +30,43 @@ public class Task16_KelimeOyunu01 {
          *          "Oyun bitti" yazin
          *          kullanicilarin puanlarini ve kazanan oyuncuyu yazdirin
          */
+        String kelime;
+        int puan1 = 0;
+        int puan2 = 0;
+        boolean oyunDevam = true;
 
+        System.out.println("1. oyuncu kelimeyi gir :");
+         kelime = input.nextLine();
+
+         boolean oyuncu1Sirada = true;
+
+        while (oyunDevam) {
+            System.out.println("kelime = " + kelime);
+            System.out.println("2.oyuncu kabul ediyor musun ? (E/H)");
+          String  kabul = input.next();
+            if(kabul.equalsIgnoreCase("E")){
+                System.out.println("2. oyuncu kelimeyi kabul etti");
+            }else System.out.println("Kabul edilmedi");
+
+
+            if(oyuncu1Sirada) {
+                System.out.println("1. oyuncu kazandi!");
+            }else System.out.println("2.oyuncu kazandi");
+            break;
+        }
+
+if(oyuncu1Sirada){
+    puan1+=kelime.length();
+}else {
+    puan2 += kelime.length();
+}
+        System.out.print("Oyuna devam etmek istiyor musunuz? (E/H): ");
+        String devam = input.nextLine();
+
+        if (!devam.equalsIgnoreCase("E")) {
+            System.out.println("Oyun bitti!");
+           
+        }
 
     }
 }
