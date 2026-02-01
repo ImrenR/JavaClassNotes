6b382b4ecb77fdf07a5379b8151222bc1aaa26eb
ImrenR <imrenrahbay@gmail.com>
2026-02-01 21:05:53 +0000

    OCAtasks

--- src/j10_MethodCreation/Tasks/Task23.java
+++ src/j10_MethodCreation/Tasks/Task23.java
@@ -3,6 +3,7 @@ package j10_MethodCreation.Tasks;
 import java.util.Scanner;
 
 public class Task23 {
+
     static Scanner input = new Scanner(System.in);//gunes eyledim ki herkeşler erişebile
 
     public static void main(String[] args) {
@@ -17,11 +18,23 @@ public class Task23 {
          */
 
 
-
-
+vucutKitleInxsi(34.7);
+       double sayi= input.nextInt();
     }//main sonu
 
+public static void vucutKitleInxsi(double sayi){
 
+        if(sayi>0 && sayi <= 18.5) {
+            System.out.println("Zayif");
+        } else if (sayi >=18.5 && sayi<= 24.9) {
+            System.out.println("Normal agirliktadir");
+
+        } else if (sayi>=25.0 && sayi<=29.9) {
+            System.out.println("Kilolusunuz");
+        } else if (sayi >= 30.0) {
+            System.out.println("Obezsiniz");
+        }
+}
 
 
 }
