0b7f3b6e3f2cd1166e24e6b1f30182473185b9bc
ImrenR <imrenrahbay@gmail.com>
2026-02-01 17:20:33 +0000

    OCAtasks

--- src/j10_MethodCreation/Tasks/Task19.java
+++ src/j10_MethodCreation/Tasks/Task19.java
@@ -10,8 +10,30 @@ public class Task19 {
 
     */
 
+printEt();
+
+
+    }
+
+    private static void printEt(){
+
+
+        char one = 'a' / 'a'; // 1
+        char two = (char) (one + one); // 2
+        char three = (char) (one + two); // 3
+        char four = (char) (two + two); // 4
+        char five = (char) (two + three); // 5
+        char six = (char) (five + one); // 6
+        char seven = (char) (five + two); // 7
+        char eight = (char) (five + three); // 8
+        char nine = (char) (five + four); // 9
+        char ten = (char) (five + five); // 10
 
 
 
+        // 1'den 100'e kadar sayıları yazdırıyoruz
+        for (int i = one; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
