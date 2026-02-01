f1cd78ac70cb28ebcc0d5c66e7ef2159e68f3bea
ImrenR <imrenrahbay@gmail.com>
2026-02-01 20:59:42 +0000

    OCAtasks

--- src/j10_MethodCreation/Tasks/Task21.java
+++ src/j10_MethodCreation/Tasks/Task21.java
@@ -5,7 +5,21 @@ public class Task21 {
         /*
     task-> girilen bir tamsayının basamak sayısını print eden METHOD create ediniz.
     */
-
-
+basamakSayisiYazdir(1234);
     }
+    public static void basamakSayisiYazdir(int sayi) {
+        sayi = Math.abs(sayi);
+        int basamakSaysi=0;
+
+        if(sayi == 0) {
+            basamakSaysi =1;
+        }else {
+            while(sayi>0) {
+                sayi /=10;
+                basamakSaysi++;
+            }
+        }
+        System.out.println("basamakSaysi = " + basamakSaysi);
+    }
+
 }
