32035b3ee4c070c125b40ec7c1fd08c26dccd415
ImrenR <imrenrahbay@gmail.com>
2026-02-02 13:16:31 +0000

    tasks

--- src/j12_ArrayList/Tasks/_06_arraylist6.java
+++ src/j12_ArrayList/Tasks/_06_arraylist6.java
@@ -2,6 +2,7 @@ package j12_ArrayList.Tasks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class _06_arraylist6 {
 
@@ -17,7 +18,20 @@ public class _06_arraylist6 {
      */
 
     public static void main(String[] args) {
+List<Integer> arr = new ArrayList<>(Arrays.asList(5,4,6,2,1));
+int hill =hillNum(arr);
+        System.out.println("hill = " + hill);
+    }
 
 
+    private static int hillNum(List<Integer> arr) {
+        for (int i = 1; i < arr.size()-1; i++) {
+            if (arr.get(i) > arr.get(i-1) && arr.get(i)> arr.get(i+1)) {
+
+                return arr.get(i);
+            }
+
+        }
+        return -1;
     }
 }
