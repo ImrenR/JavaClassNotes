e846fdc6443c2b1b0e773b3eecb6ff13d1a177f9
ImrenR <imrenrahbay@gmail.com>
2026-02-02 10:45:47 +0000

    tasks

--- src/j11_Arrays/Tasks/_21_Array_reverse.java
+++ src/j11_Arrays/Tasks/_21_Array_reverse.java
@@ -1,5 +1,7 @@
 package j11_Arrays.Tasks;
 
+import java.util.Scanner;
+
 public class _21_Array_reverse {
 
     public static void main(String[] args) {
@@ -12,9 +14,20 @@ public class _21_Array_reverse {
          */
 
         //Kodu aşağıya yazınız.
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+// Ters çevirme işlemi
 
+        int reversedNumber = 0;
 
+        while (number != 0) {
+            int digit = number % 10; // Son basamağı al
+            reversedNumber = reversedNumber * 10 + digit; // Ters çevir
+            number /= 10; // Son basamağı çıkar
+        }
 
+        // Sonucu yazdır
+        System.out.println("Ters çevrilmiş sayı: " + reversedNumber);
 
 
 
