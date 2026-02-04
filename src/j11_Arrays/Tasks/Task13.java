6e70fab910a983b0dbb691327b2d9e239da9f4cd
ImrenR <imrenrahbay@gmail.com>
2026-02-04 16:53:10 +0000

    tasks

--- src/j11_Arrays/Tasks/Task13.java
+++ src/j11_Arrays/Tasks/Task13.java
@@ -7,7 +7,8 @@ public class Task13 {
     public static void main(String[] args) {
         /* TASK :
         Girilen 8 elemanli arrayin icinde
-        3 e bolunebilen sayi adedini print eden  code create ediniz (negatif sayilar dahil )
+        3 e bolunebilen sayi adedini print eden  code create ediniz
+         (negatif sayilar dahil )
 
          */
 
