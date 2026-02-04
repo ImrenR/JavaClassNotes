aa89b9cf4faff9d5ba0cf6033196b971b4c75c97
ImrenR <imrenrahbay@gmail.com>
2026-02-04 16:37:39 +0000

    tasks

--- src/j11_Arrays/Tasks/Task11.java
+++ src/j11_Arrays/Tasks/Task11.java
@@ -6,10 +6,29 @@ public class Task11 {
     public static void main(String[] args) {
         /*
 		 TASK :
-		 $ ve  £  isareti olanlarin toplamlarini ayri ayri bulup print eden code create ediniz.
+		 $ ve  £  isareti olanlarin toplamlarini ayri ayri bulup
+		 print eden code create ediniz.
         String str = "$1 $23 $134 $2 $7 $24 £32 £40 £2 £65 £4 ";
         */
         String str = "$1 $23 $134 $2 $7 $24 £32 £40 £2 £65 £4 ";
 
+        int dolarToplam = 0;
+ int euroToplam = 0;
+
+String [] newArr = str.split(" ");
+        System.out.println(Arrays.toString(newArr));
+
+        for (int i = 0; i < newArr.length; i++) {
+            if(newArr[i].startsWith("$")){
+              String sayi = newArr[i].replace("$", "");
+              dolarToplam += Integer.parseInt(sayi);
+            } else if (newArr[i].startsWith("£")) {
+                String sayi1= newArr[i].replace("£","");
+                euroToplam += Integer.parseInt(sayi1);
+            }
+
+        }
+        System.out.println("dolarToplam =" + dolarToplam);
+        System.out.println("euroToplam =" +euroToplam);
     }
 }
