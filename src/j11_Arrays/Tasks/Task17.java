18f548662b4e427fc70b64e558d37c626651a99f
ImrenR <imrenrahbay@gmail.com>
2026-02-04 20:52:15 +0000

    tasks

--- src/j11_Arrays/Tasks/Task17.java
+++ src/j11_Arrays/Tasks/Task17.java
@@ -12,7 +12,16 @@ public class Task17 {
                 {6,1,2,2},//2. kat
         };
 
+int enBuyukEleman =arr[0][0];
 
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+             if(arr[i][j] >enBuyukEleman) {
+                 enBuyukEleman = arr[i][j];
+             }
+            }
+        }
+        System.out.println("enBuyukEleman = " + enBuyukEleman); // 6
 
     }
 }
