857f2dec4df8a4058db7b4a2fa0a8dde703e75b3
ImrenR <imrenrahbay@gmail.com>
2026-02-04 16:19:39 +0000

    arraysRecap

--- src/j11_Arrays/Tasks/Task09.java
+++ src/j11_Arrays/Tasks/Task09.java
@@ -4,8 +4,21 @@ import java.util.Scanner;
 
 public class Task09 {
     public static void main(String[] args) {
-        // Task -> Girilen 5 sayıyı , giriş sırasına göre tersten print eden code create ediniz
+        // Task -> Girilen 5 sayıyı ,
+        // giriş sırasına göre tersten print eden code create ediniz
 
+Scanner input = new Scanner(System.in);
+        System.out.println("5 adet sayi giriniz : ");
+        int sayi1=input.nextInt();
+        int sayi2=input.nextInt();
+        int sayi3=input.nextInt();
+        int sayi4=input.nextInt();
+        int sayi5=input.nextInt();
 
+     int [] arr = {sayi1,sayi2,sayi3,sayi4,sayi5};
+
+        for (int i = arr.length-1; i >=0 ; i--) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
