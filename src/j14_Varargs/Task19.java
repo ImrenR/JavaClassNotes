327373051e3479790062975b02a445a4829c9276
ImrenR <imrenrahbay@gmail.com>
2026-02-07 04:15:53 +0000

    tasks

--- src/j14_Varargs/Task19.java
+++ src/j14_Varargs/Task19.java
@@ -1,16 +1,32 @@
 package j14_Varargs;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task19 {
     static void main() {
-        //Create a method removeDuplicates that accepts varargs of integers and returns an ArrayList<Integer> containing only unique values.
-        //
+        //Create a method removeDuplicates that accepts varargs
+        // of integers and
+        // returns an ArrayList<Integer containing only unique values.
         //Rules:
-        //
         //Do not use Set
-        //
         //Use loops and contains()
-        //
         //Concepts combined:
         //varargs, ArrayList, loops, if statements
+
+        List<Integer> uniqueNumbers = removeDuplicates(1,3,4,3,5,6,1,6);
+        System.out.println("uniqueNumbers = " + uniqueNumbers);
+    }
+
+    private static List<Integer> removeDuplicates(int...numbers){
+        List<Integer> list= new ArrayList<>();
+
+        for (int num: numbers) {
+            if(!list.contains(num)) {
+                list.add(num);
+            }
+        }
+        return list;
     }
 }
--- src/j14_Varargs/Task20.java
+++ src/j14_Varargs/Task20.java
@@ -1,5 +1,8 @@
 package j14_Varargs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task20 {
     static void main() {
         //Create a method analyzeSentence that accepts varargs of Strings representing words.
@@ -13,5 +16,35 @@ public class Task20 {
         //The longest word
         //
         //Words that start with a vowel
+
+
+        String yeniListe = analyzeSentence("Imren", "Ali", "Unzile", "Berfu", "lolo");
+        System.out.println("yeniListe = " + yeniListe);
+    }
+
+
+    private static String analyzeSentence(String... isimler) {
+        int uzunKelimaSayisi = 0;
+        String enUzunKelime = "";
+        int toplamKelime = isimler.length;
+        List<String> sesliIleBaslayanlar = new ArrayList<>();
+        for (String avuc : isimler) {
+
+            if (4 < avuc.length()) {
+                uzunKelimaSayisi++;
+
+            }
+            if (avuc.length() > enUzunKelime.length()) {
+                enUzunKelime = avuc;
+            }
+            char ilkHarf = Character.toLowerCase(avuc.charAt(0));
+            if ("aeiou".indexOf(ilkHarf) >= 0) {
+                sesliIleBaslayanlar.add(avuc);
+            }
+        }
+
+        String sonuc = "toplam kelime sayisi : " + toplamKelime + "4 karakterden uzun kelime" + uzunKelimaSayisi
+                + " en uzun kelime : " + enUzunKelime + "Sesli harfle baslayab kelimeler : " + sesliIleBaslayanlar;
+        return sonuc;
     }
 }
