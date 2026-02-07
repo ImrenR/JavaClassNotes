1a7ef718cb639adf4ae225ab93500c2560181a78
ImrenR <imrenrahbay@gmail.com>
2026-02-07 03:24:50 +0000

    tasks

--- src/j12_ArrayList/Tasks/Task17.java
+++ src/j12_ArrayList/Tasks/Task17.java
@@ -2,6 +2,7 @@ package j12_ArrayList.Tasks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Task17 {
     public static void main(String[] args) {
@@ -9,10 +10,26 @@ public class Task17 {
         // input : [1, 3, 5, 3, 5, 6, 1, 7]
         // output [1, 3, 5, 6, 7]
 
+        List<Integer> sayilar = new ArrayList<>(
+                Arrays.asList(1, 3, 5, 3, 5, 6, 1, 7)
+        );
+        List<Integer> sonuc = tekrarEtmeyenListe(sayilar);
+
+        System.out.println(sonuc);
+
 
     }//main sonu
 
+    private static List<Integer> tekrarEtmeyenListe(List<Integer> sayilar) {
+    List<Integer> yeniListe = new ArrayList<>();
 
+    for(Integer sayi: sayilar) {
+        if(!yeniListe.contains(sayi)){
+            yeniListe.add(sayi);
+        }
+    }
+    return yeniListe;
+    }
 
 
 }//class sonu
