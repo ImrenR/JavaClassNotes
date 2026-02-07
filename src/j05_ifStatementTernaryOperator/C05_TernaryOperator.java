6d694aec62538f14578f2c73f11e39ec31104c70
ImrenR <imrenrahbay@gmail.com>
2026-02-07 19:32:59 +0000

    tasks

--- src/j05_ifStatementTernaryOperator/C05_TernaryOperator.java
+++ src/j05_ifStatementTernaryOperator/C05_TernaryOperator.java
@@ -8,7 +8,15 @@ public class C05_TernaryOperator {
         // Task-> girilen bir tamsayının ciftse yarısını  değilse "agıdeşşş tek'in yarısı tam olmazzz" print eden code create ediniz.
 
 
+Scanner input = new Scanner(System.in);
+        System.out.println("Bir tamsayi giriniz :");
+        int sayi = input.nextInt();
+        int toplam = 0;
 
+        if(sayi%2 ==0){
+            toplam +=sayi/2;
+            System.out.println("toplam = " + toplam);
+        }else System.out.println("agıdeşşş tek'in yarısı tam olmazzz");
 
 
 
