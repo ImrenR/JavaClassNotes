9592e9bdc65b3ea58333bcf8bbe32525f5385e7b
ImrenR <imrenrahbay@gmail.com>
2026-02-07 03:05:39 +0000

    tasks

--- src/j12_ArrayList/Tasks/Task14.java
+++ src/j12_ArrayList/Tasks/Task14.java
@@ -3,11 +3,16 @@ package j12_ArrayList.Tasks;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Task14 {
     public static void main(String[] args) {
  // Task-> Kullancıdan alınan str'nin karakter saysını print eden code create ediniz.
 
+Scanner input= new Scanner(System.in);
+        System.out.println("Bir kelime giriniz : ");
+        String str = input.nextLine();
 
+        System.out.println("str.length() = " + str.length());
     }
 }
