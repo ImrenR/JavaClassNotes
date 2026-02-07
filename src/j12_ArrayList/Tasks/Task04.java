2646538c4146f8eea3351f45510ddf1fcd690f09
ImrenR <imrenrahbay@gmail.com>
2026-02-07 01:40:09 +0000

    tasks

--- src/j12_ArrayList/Tasks/Task04.java
+++ src/j12_ArrayList/Tasks/Task04.java
@@ -2,6 +2,7 @@ package j12_ArrayList.Tasks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Task04 {
     public static void main(String[] args) {
@@ -13,7 +14,17 @@ public class Task04 {
          * INPUT : list1={"Ali","Veli","Ayse","Fatma","Omer"}
          * OUTPUT : [Veli,Omer]
          */
+ArrayList<String> list1 = new ArrayList<>(
+        Arrays.asList("Ali","Veli","Ayse","Fatma","Omer")
+);
+        Iterator<String> itr = list1.iterator();
 
-
+        while(itr.hasNext()) { // sirada eleman var mi
+            String isim = itr.next(); // siradaki elemani getir
+            if(isim.toLowerCase().contains("a")) {
+                itr.remove(); // son getirilen elemani sil
+            }
+        }
+        System.out.println(list1);
     }
 }
