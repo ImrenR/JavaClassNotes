4e6552d538602d9eae5486e9f3899d3c15ddb5c5
ImrenR <imrenrahbay@gmail.com>
2026-02-07 20:01:35 +0000

    tasks

--- src/j06_SwitchStatement/tasks/Task06.java
+++ src/j06_SwitchStatement/tasks/Task06.java
@@ -5,8 +5,26 @@ import java.util.Scanner;
 public class Task06 {
 
     public static void main(String[] args) {
-        //Task-> Girilen  VIP (Very Important Person) kisaltmasindaki harflerin anlamini print eden code create ediniz
+        //Task-> Girilen  VIP (Very Important Person)
+        // kisaltmasindaki harflerin anlamini print eden code create ediniz
 
+Scanner input = new Scanner(System.in);
+        System.out.println("VIP de ogrenmek istediginiz harf acilimini giriniz");
+        String harf = input.next();
 
+        switch (harf) {
+            case "V":
+                System.out.println("Very");
+                break;
+            case "I" :
+                System.out.println("Important");
+                break;
+            case"P":
+                System.out.println("Person");
+                break;
+            default:
+                System.out.println("GEcersiz bir harf girdiniz");
+                break;
+        }
     }
 }
--- src/j06_SwitchStatement/tasks/Task07.java
+++ src/j06_SwitchStatement/tasks/Task07.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Task07 {
     public static void main(String[] args) {
-        // Task-> Girilen 3 basamaklı bir sayıyı harf karakteri ile print eden code create ediniz.
+        // Task-> Girilen 3 basamaklı bir sayıyı harf karakteri
+        // ile print eden code create ediniz.
 
     }
 }
