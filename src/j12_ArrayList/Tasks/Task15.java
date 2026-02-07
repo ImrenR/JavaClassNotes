1f55a840728c6eefafd337b3de4d2f35c63c00af
ImrenR <imrenrahbay@gmail.com>
2026-02-07 03:15:36 +0000

    tasks

--- src/j12_ArrayList/Tasks/Task15.java
+++ src/j12_ArrayList/Tasks/Task15.java
@@ -10,5 +10,25 @@ public class Task15 {
      */
     public static void main(String[] args) {
 
+        System.out.println("100 den kucuk asal sayilar : ");
+
+        for (int i = 2; i < 100; i++) { // 2 den baslariz
+
+            if(isAsal(i)){
+                System.out.println("i = " + i);
+            }
+        }
+    }
+
+    private static boolean isAsal(int n) {
+        if(n < 2)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n) ; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+
+        }
+        return true;
     }
 }
