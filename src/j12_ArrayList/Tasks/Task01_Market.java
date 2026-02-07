0b6eb9df27bf23cc7c4dde75621c428bc289b06c
ImrenR <imrenrahbay@gmail.com>
2026-02-07 00:57:44 +0000

    tasks

--- src/j12_ArrayList/Tasks/Task01_Market.java
+++ src/j12_ArrayList/Tasks/Task01_Market.java
@@ -29,68 +29,89 @@ public class Task01_Market {
 
     public static void main(String[] args) {
 
-List<String> gunler = new ArrayList<>(Arrays.asList("Pazartesi", "Sali", "Carsamba", "Persembe","Cuma","Cumartesi","Pazar"));
-List<Double> gunlukKazanclar= new ArrayList<>();
+        //1. Step
+List<String> days = new ArrayList<>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+
+       // 2. Step
+List<Double> gunlukKazanclar = new ArrayList<>();
 
 Scanner scan= new Scanner(System.in);
 
-// 3. Adim Kullanicidan 7 gunluk kazanci al
+// 3. Step get from user the daily earns
         
-        int i=0;
-        while (i < gunler.size()) {
-            System.out.println( gunler.get(i) + ". kazancini giriniz :" );
-            gunlukKazanclar.add(scan.nextDouble());
-            i++;
-        }
-        
-        // Ortalama
-        
-        double ortalama = getOrtalamaKazancin(gunlukKazanclar);
-        System.out.println("ortalama = " + ortalama);
-        // Ortalamanin ustu 
+int i =0;
 
-        System.out.println( "Ortalamanin ustunde kazanc olan gunler: " + getOrtalamaninUstundeKAzancGunler(gunler,gunlukKazanclar,ortalama));
-    // ortalamanin alti
-
-        System.out.println( "Ortalama" + getOrtalamaninAltiKAzancGunleri(gunler,gunlukKazanclar,ortalama));
-
-
-
-    }
-
-    private static List<String> getOrtalamaninAltiKAzancGunleri(List<String> gunler, List<Double> gunlukKazanclar, double ortalama) {
-List<String> result = new ArrayList<>();
-
-        for (int i = 0; i < gunlukKazanclar.size(); i++) {
-            if (gunlukKazanclar.get(i)< ortalama) {
-                result.add(gunler.get(i));
-            }
-        }
-return result;
-    }
-
-    private static List<String> getOrtalamaninUstundeKAzancGunler(List<String> gunler, List<Double> gunlukKazanclar, double ortalama) {
-    List<String> result = new ArrayList<>();
-        for (int i = 0; i < gunlukKazanclar.size(); i++) {
-            if(gunlukKazanclar.get(i) > ortalama) {
-                result.add(gunler.get(i));
-            }
-
-        }
-        return result;
-
-
-    }
-
-    private static double getOrtalamaKazancin(List<Double> gunlukKazanclar) {
-    double toplam =0;
-    for (double each:gunlukKazanclar) {
-        toplam +=each;
-    }
-
-   return toplam/gunlukKazanclar.size();
-
-    }
-
-
+while (i< days.size()) {
+    System.out.println(days.get(i) + ". gunun kazancini giriniz :");
+    gunlukKazanclar.add(scan.nextDouble());
+    i++;
 }
+        
+        // Average
+double average =getOrtalamaKazancim(gunlukKazanclar,days);
+        System.out.println("ortalama = " + average);
+
+        // The days above average
+
+        System.out.println("The daily earns above average= " + getOrtalamaninUstuGunler(average, days, gunlukKazanclar));
+
+        //The days below average
+
+        System.out.println("The daily earns below average= " + getOrtlamaninAltindaOlanGunler(average, days, gunlukKazanclar));
+
+
+    }
+
+    private static List<String> getOrtlamaninAltindaOlanGunler(double ortalama, List<String> gunler, List<Double> gunlukKazanclar) {
+
+        List<String> ortAltindakiGunler = new ArrayList<>();
+
+        for (int i = 0; i < gunler.size(); i++) {
+            if(ortalama> gunlukKazanclar.get(i)) {
+                ortAltindakiGunler.add(gunler.get(i));
+            }
+        }
+        return ortAltindakiGunler;
+    }
+
+    private static List<String> getOrtalamaninUstuGunler(double ortalama, List<String> gunler, List<Double> gunlukKazanclar) {
+        List<String> ortUstuGunler = new ArrayList<>();
+
+        for (int i = 0; i < gunler.size(); i++) {
+            if(ortalama< gunlukKazanclar.get(i)) {
+                ortUstuGunler.add(gunler.get(i));
+            }
+        }
+        return ortUstuGunler;
+    }
+
+    private static double getOrtalamaKazancim(List<Double> gunlukKazanclar, List<String> gunler) {
+            int toplam=0;
+            for(Double avuc: gunlukKazanclar) {
+                toplam+=avuc;
+        }
+
+        return  toplam /gunler.size();
+    }
+
+
+}/// Output
+
+///  Monday. gunun kazancini giriniz :
+ //       100
+///  Tuesday. gunun kazancini giriniz :
+//        134.6
+///  Wednesday. gunun kazancini giriniz :
+//        456.7
+///   Thursday. gunun kazancini giriniz :
+//        45.3
+///   Friday. gunun kazancini giriniz :
+//        78.4
+///   Saturday. gunun kazancini giriniz :
+//          34
+///   Sunday. gunun kazancini giriniz :
+//        12
+///  average = 122.0
+///  The daily earns above average = [Tuesday, Wednesday]
+/// The daily earns below average = [Monday, Thursday, Friday, Saturday, Sunday]
+
