25498e5dcb620b0bd4074fcd70056e9cc313b376
ImrenR <imrenrahbay@gmail.com>
2026-02-07 03:29:37 +0000

    tasks

--- src/j12_ArrayList/Tasks/Task18.java
+++ src/j12_ArrayList/Tasks/Task18.java
@@ -20,9 +20,25 @@ public class Task18 {
         sayilar.add(1);
         sayilar.add(7);
 
+        System.out.println("sayilar = " + sayilar);
+        // Ornek : 1. ve 4. index i degistir
+
+        swapIndexes(sayilar,1,4);
+        System.out.println("sayilar = " + sayilar);
 
     }
 
+    private static void swapIndexes(List<Integer> sayilar, int index1, int index2) {
+        if(index1 < 0 || index2 <0 || index1 >= sayilar.size() || index2 >= sayilar.size()){
+            System.out.println("Hatali index girisi!");
+            return;
+        }
+
+        Integer temp = sayilar.get(index1);
+        sayilar.set(index1,sayilar.get(index2));
+        sayilar.set(index2,temp);
+
+    }
 
 
 }
