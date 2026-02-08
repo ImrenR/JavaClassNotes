b97d5759384dcc08fa011d95708f82acf17f17c3
ImrenR <imrenrahbay@gmail.com>
2026-02-08 17:16:47 +0000

    tasks

--- src/j09_Break_Continue/C04_PinControl.java
+++ src/j09_Break_Continue/C04_PinControl.java
@@ -4,10 +4,29 @@ import java.util.Scanner;
 
 public class C04_PinControl {
     public static void main(String[] args) {
-        // Task -> Girilen String data type bir pin datasının 3 giriş hakkında kontrol eden code create ediniz.
-        Scanner input = new Scanner(System.in);
+        // Task -> Girilen String data type bir
+        // pin datasının 3 giriş hakkında kontrol eden code create ediniz.
 
+       String dogruPin= "12345";
+       int girisHakki= 3;
 
+       Scanner input= new Scanner(System.in);
 
+       while (girisHakki>0) {
+           System.out.println("Bir pin kodu giriniz : ");
+           String pin = input.nextLine();
+            if(pin.equals(dogruPin)) {
+                System.out.println("Giris basarili");
+                break;
+            }else {
+                girisHakki--;
+                System.out.println("Yanlis pin, kalan hak" + girisHakki + "tekrar deneyiniz");
+
+            }
+       }
+
+       if(girisHakki==0){
+           System.out.println("Kartiniz bloke edildi");
+       }
     }
 }
