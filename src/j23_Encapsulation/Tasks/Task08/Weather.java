006d53575373ee916badda839925c959ea270fc9
ImrenR <imrenrahbay@gmail.com>
2026-02-10 11:48:25 +0000

    taskEncap

--- src/j23_Encapsulation/Tasks/Task08/Weather.java
+++ src/j23_Encapsulation/Tasks/Task08/Weather.java
@@ -1,8 +1,56 @@
 package j23_Encapsulation.Tasks.Task08;
 
 public class Weather {
+
+  private double temperatureCelsius;
+
+    public Weather(double temperatureCelsius) {
+        this.temperatureCelsius = temperatureCelsius;
+
+    }
+
+    public double getTemperatureCelsius() {
+        return temperatureCelsius;
+    }
+
+    public void setTemperatureCelsius(double temperatureCelsius) {
+        this.temperatureCelsius = temperatureCelsius;
+    }
+
+    // method c-> f cevir
+    public double getTemperatureFahrenheit(){
+        return (temperatureCelsius*9 /5) + 32;
+    }
+    // status
+    public String getWeatherStatus(){
+        if(temperatureCelsius<0) {
+           return("Freezing");
+        } else if (temperatureCelsius<15 && temperatureCelsius>0) {
+            return("Cold");
+        } else if (temperatureCelsius>16 && temperatureCelsius<25) {
+            return("Mild");
+        }else return("Hot");
+    }
+
+    @Override
+    public String toString() {
+        return "Weather{" +
+                "temperatureCelsius=" + temperatureCelsius +
+                ", Fahrenheit=" + getTemperatureFahrenheit() +
+                ", Status='" + getWeatherStatus() + '\'' +
+                '}';
+    }
 }
 
+
 class WeatherRunner{
+    static void main() {
+        Weather w1= new Weather(-5);
+        w1.getWeatherStatus();
+        w1.getTemperatureFahrenheit();
+        System.out.println("w1 = " + w1);
+    }
+
+
 
 }
