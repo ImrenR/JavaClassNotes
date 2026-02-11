8dca8014d00efbae71725d4e546a8d3512a51591
ImrenR <imrenrahbay@gmail.com>
2026-02-11 15:32:55 +0000

    Exception Tasks

--- src/j25_Exceptions/Tasks/Task04/Clarusway.java
+++ src/j25_Exceptions/Tasks/Task04/Clarusway.java
@@ -4,8 +4,45 @@ import java.util.ArrayList;
 
 public class Clarusway {
 
+private String okulAd;
+private int maxOgrenciSayisi;
+private ArrayList<It> ogrenciler;
 
 
+
+
+    public Clarusway(String okulAd, int maxOgrenciSayisi) {
+        this.okulAd = okulAd;
+        this.maxOgrenciSayisi = maxOgrenciSayisi;
+        this.ogrenciler=new ArrayList<>();
+
+    }
+
+    public  void ogrenciEkle(It ogrenci){
+        ogrenciler.add(ogrenci);
+    }
+
+    public boolean isOkulDolu(){
+        return ogrenciler.size() >= maxOgrenciSayisi;
+    }
+
+
+    public ArrayList<It> getOgrenciler() {
+        return ogrenciler;
+    }
+
+    public String getOkulAd() {
+        return okulAd;
+    }
+
+    @Override
+    public String toString() {
+        return "Clarusway{" +
+                "okulAd='" + okulAd + '\'' +
+                ", maxOgrenciSayisi=" + maxOgrenciSayisi +
+                ", ogrenciler=" + ogrenciler +
+                '}';
+    }
 }
 
 
---
+++ src/j25_Exceptions/Tasks/Task04/YasException.java
@@ -0,0 +1,8 @@
+package j25_Exceptions.Tasks.Task04;
+
+public class YasException extends Exception{
+
+    public YasException(String message){
+        super(message);
+    }
+}
