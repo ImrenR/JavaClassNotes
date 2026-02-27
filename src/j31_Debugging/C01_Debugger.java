35b331ed41fd869635671694c3b550327f31edc5
ImrenR <imrenrahbay@gmail.com>
2026-02-27 21:07:36 +0000

    Debugging

--- src/j31_Debugging/C01_Debugger.java
+++ src/j31_Debugging/C01_Debugger.java
@@ -14,7 +14,7 @@ public class C01_Debugger {
  her iki durumda da debugger çalıştırmamaya dikkat edilmeli.
  Amac sıfır debug aktivitesi ile uygulamayı istenilen seviyeye getirebilmek.
  */
-
+static int ebikgabik =17;
     public static void main(String[] args) {
 method01();
     }//main sonu
@@ -22,8 +22,13 @@ method01();
     public static void method01() {
         for (int i = 0; i < 10 ; i++) {
             System.out.println(" **** method-01 START **** "); // meth entry
+ebikgabik++;
+            int a=0;
             System.out.print(i + " ");
+            a+=2*i;
         }
+
+
         System.out.println(); // dumy
         System.out.println(" **** method-02 CALL EDILDI **** ");
         method02();
@@ -35,6 +40,7 @@ method01();
     public static void method02() {
         for (int x = 0; x < 7 ; x++) {
             System.out.println(" **** method-02 START **** ");
+            ebikgabik++;
             System.out.println(x + " ");
         }
         System.out.println(); // dumy
@@ -44,6 +50,7 @@ method01();
     public static void method03() {
         for (int y = 0; y < 5 ; y++) {
             System.out.println(" **** method-03 START **** ");
+            ebikgabik--;
             System.out.println(y + " ");
         }
         System.out.println(); // dumy
